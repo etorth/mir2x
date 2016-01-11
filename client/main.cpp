f5ce515fe27cfbe3e691ca85eafe0647e3d769e4
anhong <anhong@deb8200>
2016-01-11 07:57:44 -0800

    start client part

---
+++ client/main.cpp
@@ -0,0 +1,12 @@
+#include "game.hpp"
+
+int main(int argc, char* argv[])
+{
+    Game::StartSystem();
+
+    Game stGame;
+    stGame.Init();
+    stGame.MainLoop();
+    stGame.Clear();
+	return 0;
+}
