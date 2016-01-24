2a75c0f259aa62d2efd98a6dff0241045072ebef
Anhong <ahe01@syr.edu>
2016-01-24 02:09:14 -0800

    Update sendmessage.hpp

--- common/sendmessage.hpp
+++ common/sendmessage.hpp
@@ -1,2 +1,18 @@
 #pragma once
-void SendMessage(uint16_t, void *);
+#include "mir2xasio.hpp"
+
+template<typename MsgType>
+void SendMessage(const MsgType & stMsg, std::function<void(uint8_t *, size_t)> fnSendBuf)
+{
+    std::static_assert(sizeof(stMsg) < 255 + 2);
+    size_t nSize = sizeof(stMsg) - 2;
+    uint8_t chMsgID1 = (uint8_t)((stMsg.ID & 0X00FF));
+    uint8_t chMsgID2 = (uint8_t)((stMsg.ID & 0XFF00) >> 8);
+    
+    fnSendBuf(&chMsgID1, 1);
+    if(chMsgID2 != 0){
+        std::static_assert(sizeof(MsgType) < 255 + 2);
+        fnSendBuf(sizeof(MsgType) - 2, 1);
+        fnSendBuf()
+    }
+}
