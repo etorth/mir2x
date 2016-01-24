89e7ed5b03faeb2305c5fc148a7c87b994c0e4ff
Anhong <ahe01@syr.edu>
2016-01-24 00:19:36 -0800

    Update processnetevent.cpp

--- client/processnetevent.cpp
+++ client/processnetevent.cpp
@@ -18,6 +18,19 @@
  * =====================================================================================
  */
 
-void Game::ProcessEvent(uint32_t nCode, uint32_t nData1, void *pData2)
+void Game::ProcessEvent(uint32_t nCode, uint32_t nData1, uint8_t *pData2)
 {
+    uint8_t B1InCode = (uint8_t)((nCode & 0X000000FF) >>  0);
+    uint8_t B2InCode = (uint8_t)((nCode & 0X0000FF00) >>  8);
+    uint8_t B3InCode = (uint8_t)((nCode & 0X00FF0000) >> 16);
+    
+    uint8_t chHC     = B1InCode;
+    uint8_t chSubHC  = B2InCode;
+    size_t  nDataLen = (size_t)B3InCode;
+    
+    switch(((uint16_t)(chHC) << 8) + chSubHC){
+        case MSG_PING:      NEOnPing((Uint32)nData1); break;
+        case MSG_PLAYERNUM: NEOnPlayerNum(nData1);    break;
+        default: break;
+    }
 }
