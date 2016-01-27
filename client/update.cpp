3833b5dbc74c236b45f8414d845ff071569177ca
Anhong <ahe01@syr.edu>
2016-01-27 01:10:28 -0800

    Update update.cpp

--- client/update.cpp
+++ client/update.cpp
@@ -2,4 +2,12 @@
 
 void Game::Update()
 {
-    switch(m_ProcessID){
+    switch(m_CurrentProcessID){
+        case PROCESSID_LOGO:  UpdateOnLogo(SDL_GetTicks());  DrawOnLogo();  break;
+        case PROCESSID_SYRC:  UpdateOnSyrc(SDL_GetTicks());  DrawOnSyrc();  break;
+        case PROCESSID_LOGIN: UpdateOnLogin(SDL_GetTicks()); DrawOnLogin(); break;
+        case PROCESSID_RUN:   UpdateOnRun(SDL_GetTicks());   DrawOnRun();   break;
+        default: break;
+    }
+    m_FPSCount++;
+}
