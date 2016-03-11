c57bd1826a36ebd484c6db359d4a48f35aefa7dc
anhong <anhong@deb8200>
2016-03-11 15:01:48 -0800

    add ScopeGuard

---
+++ common/src/scopeguard.hpp
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename: scopeguard.hpp
+ *        Created: 03/11/2016 14:36:20
+ *  Last Modified: 03/11/2016 14:59:17
+ *
+ *    Description: 
+ *
+ *        Version: 1.0
+ *       Revision: none
+ *       Compiler: gcc
+ *
+ *         Author: ANHONG
+ *          Email: anhonghe@gmail.com
+ *   Organization: USTC
+ *
+ * =====================================================================================
+ */
+
+
+#pragma once
+
+#include "folly/ScopeGuard.h"
