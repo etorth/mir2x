854574605c2807fe55e9e3faaff6e592621b9657
anhong <anhong@deb830>
2016-03-21 22:59:49 -0700

    think of TokenBoard implementation

--- client/src/tokenboard.hpp
+++ client/src/tokenboard.hpp
@@ -3,18 +3,87 @@
  *
  *       Filename: tokenboard.hpp
  *        Created: 06/17/2015 10:24:27 PM
- *  Last Modified: 03/21/2016 17:06:31
+ *  Last Modified: 03/21/2016 22:59:41
  *
- *    Description: TokenBoard is not editable and selectable, this class is intended to 
- *                 be designed as a button like text terminal.
+ *    Description: Design TBD.
  *
- *                 TBD:
- *                   1. whether develop an EditBoard which directly accept events
- *                      or just use set/get to tokenboard to show edits?
+ *                 For scenario we need text-emoticon mixed boards:
  *
- *                   2. is it proper to accept drag event for select in TokenBoard?
+ *                 1. button like text terminal. which we only need to support emoticon
+ *                    and eventtext, eventtext accept button click event only. This is
+ *                    the simple case. no select/edit support needed.
  *
- *                   3. WTF, I don't know
+ *                 2. chatbox, two parts:
+ *                      a. already sent chat messages.
+ *                      b. editting chatting messages.
+ *
+ *                      +--------------------------+ 
+ *                      | xxxxx :), xxxxxxxxxxxxxx |
+ *                      | xxxxxxxxxxxxxxxxxxx.     |
+ *                      | xx                       | <----- already sent messages
+ *                      | xxxxxxxxxxxxxxxxxxxxxxxx |
+ *                      | xxxxxxxxxxxxxxxxxxxxxxxx |
+ *                      | xxxxxxxxxxxxxxxxxxxxxxxx |
+ *                      +--------------------------+
+ *                      | xxx :( xxxx |            |
+ *                      |                          | <----- editting messages
+ *                      +--------------------------+
+ *
+ *                      for 2-a: there is emoticon, normal text, eventtext, eventtext for hyperlinks
+ *                               to trigger event, just like 1. 2-a should support select.
+ *
+ *                      for 2-b: there is emoticon and normal text. need to support select/edit. as
+ *                               analyzed, edit is insert, and insert can be implemented by select.
+ *                               so 2-b we need to support select.
+ *
+ *                 Analysis: why we use select to support insert:
+ *
+ *                   for insert, we add new tokens to current board, we can everytime insert a new
+ *                   token and re-padding, or we can return the underlying XML/plainText, and then
+ *                   re-generate the whole board. Re-generatioin is better since re-padding every-
+ *                   time is a re-generation. Even if we implement a new class EditBoard, it's still
+ *                   a decision for re-padding and re-generation. So just use TokenBoard.
+ *
+ *                 For boards need to support insert, we need to supprt insert at anywhere we want,
+ *                 this means we need to support select anywhere we want by click at the token of 
+ *                 the place. So problem comes with eventtext, which already take click as event tr-
+ *                 igger.
+ *
+ *                   But it's OK since for editable board, we don't need eventtext(hyperlinks), the
+ *                 place we need hyperlinks is for case 2-a. This means at which we need to trigger
+ *                 event, just trigger it, don't think about place location for insert. Since if we
+ *                 really need to insert in current board, this board doesn't need trigger event.
+ *
+ *                   Another problem, how to support insert before current cursor? We can use func-
+ *                 tion like GetXML() or GetPlainText(), but both just return formated text. inside
+ *                 the tokenboard, cursor is something like (x, y) or (section, offset). we can't do
+ *                 insert based on the returned text. Maybe in the wrapper class we can maintain a
+ *                 copy of text in the board and mapping the cursor with this copy, but this is bad
+ *                 iead. don't use it.
+ *
+ *                   Currently what I can get is inside the board class for each section I keep a
+ *                 copy of its stinrg content, this helps to return GetXML() and GetPlainText() qui-
+ *                 ckly and also we can maintain the mapping inside, and each time warpper class can
+ *                 only ask for insert a token or a piece of text. Internal text mapping will handle
+ *                 it properly, with update of string copy.
+ * 
+ *                 BUT, if we use this strategy, why not keep an external copy in the wrapper class
+ *                 and just make TokenBoard only support case 1???
+ *                   
+ *                 Event for handling or not:
+ *
+ *                 1. click, motion,  for sure we need to handle it for trigger event, for select.
+ *                 2. ctrl+c/p/x, a/b/c..., Tokenboard won't handle this. alternatively, wrapper of
+ *                    tokenboard handle it and query TokenBoard for proper behavor. IE:
+ *                          ChatBox get event:
+ *                          if it's "a":
+ *                             1. if there is selected part, replace this part with ``a"
+ *                             2. if not, put ``a" before the cursor.
+ *                          if it's click:
+ *                             1. directly pass it to tokenboard, to set cursor.
+ *                          if it's ctrl-x:
+ *                             1. query the tokenboard, if there is selected part, cut it.
+ *                             2. otherwise do nothing.
  *
  *
  *        Version: 1.0
