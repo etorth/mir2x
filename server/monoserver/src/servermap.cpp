04433cee6124f720943346a035f92ceedab4bc3d
anhong <anhong@deb830>
2016-04-07 01:44:46 -0700

    try to add object engine

--- server/monoserver/src/servermap.cpp
+++ server/monoserver/src/servermap.cpp
@@ -3,7 +3,7 @@
  *
  *       Filename: servermap.cpp
  *        Created: 04/06/2016 08:52:57 PM
- *  Last Modified: 04/06/2016 23:46:28
+ *  Last Modified: 04/07/2016 00:48:47
  *
  *    Description: 
  *
@@ -26,9 +26,9 @@ bool ServerMap::Load(const char szMapFullName)
     int nW = m_Mir2xMap.W();
     int nH = m_Mir2xMap.H();
 
-    m_ObjectList.clear();
-    m_ObjectList.resize(nH);
-    for(auto &stLine: m_ObjectList){
+    mGridObjectList.clear();
+    mGridObjectList.resize(nH);
+    for(auto &stLine: mGridObjectList){
         stLine.resize(nW);
     }
 }
@@ -58,7 +58,7 @@ bool ServerMap::CanMove(int nX, int nY, int nR)
             continue;
         }
 
-        for(auto pObject: m_ObjectList[nGridX][nGridY]){
+        for(auto pObject: mGridObjectList[nGridX][nGridY]){
             if(pObject->Type == OT_MOVINGOBJECT){
                 auto pCharObject = (CCharObject*)pObject->Object;
                 if(!pCharObject){ continue; }
@@ -117,9 +117,9 @@ int ServerMap::CheckEvent(int nX, int nY)
     return 0;
 }
 
-BOOL ServerMap::MoveToMovingObject(int nX, int nY, int nTargetX, int nTargetY, CCharObject* pObject)
+bool ServerMap::MoveToMovingObject(int nX, int nY, int nTargetX, int nTargetY, CCharObject* pObject)
 {
-    BOOL			fRet = FALSE;
+    bool			fRet = FALSE;
 
     //	CMapCellInfo*	pMapTargetCellInfo	= GetMapCellInfo(nTargetX, nTargetY);
     //	CMapCellInfo*	pMapCurrCellInfo	= GetMapCellInfo(nX, nY);
@@ -153,78 +153,18 @@ BOOL ServerMap::MoveToMovingObject(int nX, int nY, int nTargetX, int nTargetY, C
     return fRet;
 }
 
-BOOL ServerMap::RemoveObject(int nX, int nY, BYTE btType, VOID* pRemoveObject)
+
+bool ServerMap::AddNewObject(int nX, int nY, uint8_t btType, void *pNewObject)
 {
-    CMapCellInfo*	pMapCellInfo = GetMapCellInfo(nX, nY);
-    PLISTNODE		pListNode;
-    _LPTOSOBJECT	pOSObject;
+    int nGridX = nX / 48;
+    int nGridY = nY / 32;
 
-    if (pMapCellInfo)
-    {
-        //		EnterCriticalSection(&pMapCellInfo->m_cs);
-
-        if (!pMapCellInfo->m_xpObjectList) 
-            return FALSE;
-
-        if (pMapCellInfo->m_xpObjectList->GetCount())
-        {
-            pListNode = pMapCellInfo->m_xpObjectList->GetHead();
-
-            while (pListNode)
-            {
-                pOSObject = (_LPTOSOBJECT)pMapCellInfo->m_xpObjectList->GetData(pListNode);
-
-                if (pOSObject)
-                {
-                    if ((pOSObject->pObject == pRemoveObject) && (pOSObject->btType == btType))
-                    {
-                        pListNode = pMapCellInfo->m_xpObjectList->RemoveNode(pListNode);
-                        //						LeaveCriticalSection(&pMapCellInfo->m_cs);
-                        return TRUE;
-                    }
-                }
-
-                pListNode = pMapCellInfo->m_xpObjectList->GetNext(pListNode);
-            } // while (pListNode)
-        } // if (pMapCellInfo->m_pObjectList.GetCount())
-
-        //		LeaveCriticalSection(&pMapCellInfo->m_cs);
+    if(m_Mir2xMap.ValidP(nX, nY) && m_Mir2xMap.CanWalk(nGridX, nGridY)){
+        extern MonoServer *g_Monoserver;
+        m_GridObjectList[nGridY][nGridX].emplace_front(nType, pNewObject, g_Monoserver->GetTick());
+        return true;
     }
-
-    return FALSE;
-}
-
-BOOL ServerMap::AddNewObject(int nX, int nY, BYTE btType, VOID* pAddObject)
-{
-    CMapCellInfo*	pMapCellInfo = GetMapCellInfo(nX, nY);
-
-    if (pMapCellInfo)
-    {
-        if (pMapCellInfo->m_chFlag & 0x01)
-        {
-            if (!pMapCellInfo->m_xpObjectList)
-                pMapCellInfo->m_xpObjectList = new CWHList<_LPTOSOBJECT>;
-
-            _LPTOSOBJECT	pOSObject = new _TOSOBJECT;
-
-            if (pOSObject)
-            {
-                //				EnterCriticalSection(&pMapCellInfo->m_cs);
-
-                pOSObject->btType		= btType;
-                pOSObject->pObject		= pAddObject;
-                pOSObject->dwAddTime	= GetTickCount();
-
-                pMapCellInfo->m_xpObjectList->AddNewNode(pOSObject);
-
-                //				LeaveCriticalSection(&pMapCellInfo->m_cs);
-
-                return TRUE;
-            }
-        }
-    }
-
-    return FALSE;
+    return false;
 }
 
 CCharObject* ServerMap::GetObject(int nX, int nY)
@@ -363,194 +303,76 @@ void ServerMap::GetAllObject(int nX, int nY, CWHList<CCharObject*>* pList)
     } // if (pMapCellInfo)
 }
 
-int ServerMap::GetDupCount(int nX, int nY)
+ItemObject* ServerMap::GetItemObject(int nX, int nY)
 {
-    CMapCellInfo*	pMapCellInfo = GetMapCellInfo(nX, nY);
-    _LPTOSOBJECT	pOSObject;
-    PLISTNODE		pListNode;
-    int				nCount = 0;
-
-    if (pMapCellInfo)
-    {
-        //		EnterCriticalSection(&pMapCellInfo->m_cs);
-
-        if (pMapCellInfo->m_xpObjectList)
-        {
-            if (pMapCellInfo->m_xpObjectList->GetCount())
-            {
-                pListNode = pMapCellInfo->m_xpObjectList->GetHead();
-
-                while (pListNode)
-                {
-                    pOSObject = pMapCellInfo->m_xpObjectList->GetData(pListNode);
-
-                    if (pOSObject)
-                    {
-                        if (pOSObject->btType == OS_MOVINGOBJECT)
-                        {
-                            CCharObject* pCharObject = (CCharObject*)pOSObject->pObject;
-
-                            if (!pCharObject->m_fIsDead && !pCharObject->m_fIsGhost && !pCharObject->m_fHideMode)
-                                nCount++;
-                        }
-                    }
-
-                    pListNode = pMapCellInfo->m_xpObjectList->GetNext(pListNode);
-                } // while (pListNode)
-            } // if (pMapCellInfo->m_pObjectList.GetCount())
-        }
-
-        //		LeaveCriticalSection(&pMapCellInfo->m_cs);
+    if(false
+            || !m_Mir2xMap.ValidP(nX, nY)
+            || !m_Mir2xMap.CanWalk(nX / 48, nY / 32)){
+        return false;
     }
 
-    return nCount;
+    int nGirdX = nX / 48;
+    int nGridY = nY / 32;
+
+    for(auto pObject: mGridObjectList[nGridX][nGridY]){
+        if(pObject->Type == OT_ITEMOBJECT){
+            return (EventObject*)(pObject->Object);
+        }
+    }
+
+    return nullptr;
 }
 
-CMapItem* ServerMap::GetItem(int nX, int nY)
+EventObject *ServerMap::GetEventObject(int nX, int nY)
 {
-    CMapCellInfo*	pMapCellInfo = GetMapCellInfo(nX, nY);
-    _LPTOSOBJECT	pOSObject;
-    PLISTNODE		pListNode;
-
-    if (pMapCellInfo)
-    {
-        //		EnterCriticalSection(&pMapCellInfo->m_cs);
-
-        if (!(pMapCellInfo->m_chFlag & 0x01))
-            return NULL;
-
-        if (pMapCellInfo->m_xpObjectList)
-        {
-            if (pMapCellInfo->m_xpObjectList->GetCount())
-            {
-                pListNode = pMapCellInfo->m_xpObjectList->GetHead();
-
-                while (pListNode)
-                {
-                    pOSObject = pMapCellInfo->m_xpObjectList->GetData(pListNode);
-
-                    if (pOSObject)
-                    {
-                        if (pOSObject->btType == OS_ITEMOBJECT)
-                        {
-                            //							LeaveCriticalSection(&pMapCellInfo->m_cs);
-                            return (CMapItem*)pOSObject->pObject;
-                        }
-                    }
-
-                    pListNode = pMapCellInfo->m_xpObjectList->GetNext(pListNode);
-                } // while (pListNode)
-            } // if (pMapCellInfo->m_pObjectList.GetCount())
+    int nGirdX = nX / 48;
+    int nGridY = nY / 32;
+    if(m_Mir2xMap.ValidP(nX, nY) && m_Mir2xMap.CanWalk(nGridX, nGridY)){
+        auto p = std::find_if(
+                m_GridObjectList[nGridX][nGridY].begin(),
+                m_GridObjectList[nGridX][nGridY].end(),
+                [](const MapObject &rstObject) -> bool {
+                    return rstObject.Type == OT_EVENTOBJECT;
+                });
+        if(p != m_GridObjectList[nGridY][nGridX].end()){
+            return p->Object;
         }
-
-        //		LeaveCriticalSection(&pMapCellInfo->m_cs);
     }
 
-    return NULL;
+    return nullptr;
 }
 
-BOOL ServerMap::GetDropPosition(int nOrgX, int nOrgY, int nRange, int &nX, int &nY)
+bool ServerMap::CanSafeWalk(int nX, int nY)
 {
-    int nLoonCnt	= (4 * nRange) * (nRange + 1);
+    int nGirdX = nX / 48;
+    int nGridY = nY / 32;
 
-    for (int i = 0; i < nLoonCnt; i++)
-    {
-        nX = nOrgX + g_SearchTable[i].x;
-        nY = nOrgY + g_SearchTable[i].y;
-
-        if (GetItem(nX, nY) == NULL)
-            return TRUE;
+    if(m_Mir2xMap.ValidP(nX, nY) && m_Mir2xMap.CanWalk(nGridX, nGridY)){
+        if(std::find_if(
+                m_GridObjectList[nGridX][nGridY].begin(), m_GridObjectList[nGridX][nGridY].end(),
+                [nType, pObject](const MapObject &rstObject) -> bool {
+                    if((pObject->Type == OT_EVENTOBJECT)
+                            && (((EventObject*)pObject->Object)->Damage() > 0)){
+                        return false;
+                    }
+                }
+         ) != m_GridObjectList[nGridY][nGridX].end()){
+            return false;
+        }
     }
-
-    nX = nOrgX;
-    nY = nOrgY;
-
-    return FALSE;
+    return true;
 }
 
-CEvent* ServerMap::GetEvent(int nX, int nY)
+bool ServerMap::RemoveObject(int nX, int nY, uint8_t nType, void *pObject)
 {
-    CMapCellInfo*	pMapCellInfo = GetMapCellInfo(nX, nY);
-    _LPTOSOBJECT	pOSObject;
-    PLISTNODE		pListNode;
-
-    if (pMapCellInfo)
-    {
-        //		EnterCriticalSection(&pMapCellInfo->m_cs);
-
-        if (!(pMapCellInfo->m_chFlag & 0x01))
-            return NULL;
-
-        if (pMapCellInfo->m_xpObjectList)
-        {
-            if (pMapCellInfo->m_xpObjectList->GetCount())
-            {
-                pListNode = pMapCellInfo->m_xpObjectList->GetHead();
-
-                while (pListNode)
-                {
-                    pOSObject = pMapCellInfo->m_xpObjectList->GetData(pListNode);
-
-                    if (pOSObject)
-                    {
-                        if (pOSObject->btType == OS_EVENTOBJECT)
-                        {
-                            //							LeaveCriticalSection(&pMapCellInfo->m_cs);
-                            return (CEvent*)pOSObject->pObject;
-                        }
-                    }
-
-                    pListNode = pMapCellInfo->m_xpObjectList->GetNext(pListNode);
-                } // while (pListNode)
-            } // if (pMapCellInfo->m_pObjectList.GetCount())
-        }
-
-        //		LeaveCriticalSection(&pMapCellInfo->m_cs);
+    int nGirdX = nX / 48;
+    int nGridY = nY / 32;
+    if(m_Mir2xMap.ValidP(nX, nY) && m_Mir2xMap.CanWalk(nGridX, nGridY)){
+        std::remove_if(
+                m_GridObjectList[nGridX][nGridY].begin(), m_GridObjectList[nGridX][nGridY].end(),
+                [nType, pObject](const MapObject &rstObject) -> bool {
+                    return (rstObject.Type == nType && pObject == rstObject.Object);
+                }
+        );
     }
-
-    return NULL;
-}
-
-BOOL ServerMap::CanSafeWalk(int nX, int nY)
-{
-    CMapCellInfo*	pMapCellInfo = GetMapCellInfo(nX, nY);
-    _LPTOSOBJECT	pOSObject;
-    PLISTNODE		pListNode;
-
-    if (pMapCellInfo)
-    {
-        //		EnterCriticalSection(&pMapCellInfo->m_cs);
-
-        if (!(pMapCellInfo->m_chFlag & 0x01))
-            return NULL;
-
-        if (pMapCellInfo->m_xpObjectList)
-        {
-            if (pMapCellInfo->m_xpObjectList->GetCount())
-            {
-                pListNode = pMapCellInfo->m_xpObjectList->GetHead();
-
-                while (pListNode)
-                {
-                    pOSObject = pMapCellInfo->m_xpObjectList->GetData(pListNode);
-
-                    if (pOSObject)
-                    {
-                        if (pOSObject->btType == OS_EVENTOBJECT)
-                        {
-                            //							LeaveCriticalSection(&pMapCellInfo->m_cs);
-                            if (((CEvent*)pOSObject->pObject)->m_nDamage > 0)
-                                return FALSE;
-                        }
-                    }
-
-                    pListNode = pMapCellInfo->m_xpObjectList->GetNext(pListNode);
-                } // while (pListNode)
-            } // if (pMapCellInfo->m_pObjectList.GetCount())
-        }
-
-        //		LeaveCriticalSection(&pMapCellInfo->m_cs);
-    }
-
-    return TRUE;
 }
