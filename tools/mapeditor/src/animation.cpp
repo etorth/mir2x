48ab794f87eeb889985b0f2ec2ec2491eb6ecd2c
anhong <anhonghe@gmail.com>
2016-06-20 21:51:12 -0700

    work on animation for test

--- tools/mapeditor/src/animation.cpp
+++ tools/mapeditor/src/animation.cpp
@@ -3,7 +3,7 @@
  *
  *       Filename: animation.cpp
  *        Created: 06/20/2016 19:56:07
- *  Last Modified: 06/20/2016 20:17:23
+ *  Last Modified: 06/20/2016 20:55:29
  *
  *    Description: 
  *
@@ -25,33 +25,66 @@ Animation::Animation()
 {
 }
 
-void Animation::Draw()
+bool Animation::ActionValid()
 {
     if(m_Action >= m_AnimationFrameV2D.size()){
         fl_alert("internal error in Animation::Draw(): invalid action code");
-        exit(0);
+        return false;
     }
 
-    if(m_AnimationFrameV2D[m_Action].size() == 0 || m_Frame >= m_AnimationFrameV2D[m_Action].size()){
+    if(m_AnimationFrameV2D[m_Action].size() == 0){
+        fl_alert("internal error in Animation::Draw(): current action is invalid");
+        return false;
+    }
+
+    return true;
+}
+
+bool Animation::FrameValid()
+{
+    // 1. check action
+    if(!ActionValid()){ return false; }
+
+    // 2. ok check frame
+    if(m_Frame >= m_AnimationFrameV2D[m_Action].size()){
         fl_alert("internal error in Animation::Draw(): invalid frame indexing");
-        exit(0);
+        return false;
     }
 
     if(m_AnimationFrameV2D[m_Action][m_Frame].Image == nullptr){
+        if(m_AnimationFrameV2D[m_Action][m_Frame].ImageName == ""){
+            fl_alert("internal error in Animation::Draw(): invalid image name");
+            return false;
+        }
 
+        m_AnimationFrameV2D[m_Action][m_Frame].Image = Fl_Share_Image::get(m_AnimationFrameV2D[m_Action][m_Frame].ImageName.c_str());
     }
-    if(true
-            && m_AnimationFrameV2D[m_Action][m_Frame].Image == nullptr
-            && m_AnimationFrameV2D[m_Action][m_Frame].ImageName == ""){
-        fl_alert("internal error in Animation::Draw(): invalid frame indexing");
-        exit(0);
+
+    if(m_AnimationFrameV2D[m_Action][m_Frame].Image == nullptr){
+        fl_alert("internal error in Animation::Draw(): invalid image file name: %s", m_AnimationFrameV2D[m_Action][m_Frame].ImageName.c_str());
+        return false;
+    }
+
+    if(m_AnimationFrameV2D[m_Action][m_Frame].Image == nullptr){
+        if(m_AnimationFrameV2D[m_Action][m_Frame].ImageName == ""){
+            fl_alert("internal error in Animation::Draw(): invalid image name");
+            return false;
+        }
+
+        m_AnimationFrameV2D[m_Action][m_Frame].Image = Fl_Share_Image::get(m_AnimationFrameV2D[m_Action][m_Frame].ImageName.c_str());
+    }
+
+    if(m_AnimationFrameV2D[m_Action][m_Frame].Image == nullptr){
+        fl_alert("internal error in Animation::Draw(): invalid image file name: %s", m_AnimationFrameV2D[m_Action][m_Frame].ImageName.c_str());
+        return false;
     }
 
 
+    return true;
+}
 
-
-
-
-
-
+void Animation::Draw()
+{
+    // ok now we are legal to draw it
+    m_AnimationFrameV2D[m_Action][m_Frame].Image->draw(m_X + m_AnimationFrameV2D[m_Action][m_Frame].DX, m_Y + m_AnimationFrameV2D[m_Action][m_Frame].DY);
 }
