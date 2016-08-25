7680291405f236d808c8b40d0d8958c8de02ece3
Etorth <etorth@users.noreply.github.com>
2016-08-25 16:24:22 -0700

    Update widget.hpp

--- client/src/widget.hpp
+++ client/src/widget.hpp
@@ -43,7 +43,7 @@ class Widget
                 int nW = 0,                 //
                 int nH = 0,                 //
                 Widget * pWidget = nullptr, // by default all widget are independent
-                bool bFreeWidget = false)    // delete automatically when deleting its parent
+                bool bFreeWidget = false)   // delete automatically when deleting its parent
 
             : m_Parent(pWidget)
             , m_Focus(false)
