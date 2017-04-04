99de6fb15a35c930e1148c64724bdefc27eb840a
anhong <anhonghe@gmail.com>
2017-04-03 23:50:56 -0700

    work on id translate

--- tools/gfxmaker/src/main.cpp
+++ tools/gfxmaker/src/main.cpp
@@ -3,9 +3,13 @@
  *
  *       Filename: main.cpp
  *        Created: 04/03/2017 18:02:52
- *  Last Modified: 04/03/2017 18:29:44
+ *  Last Modified: 04/03/2017 23:50:07
  *
- *    Description: for file name translation
+ *    Description: for file name translation, for example
+ *                      id_translate 009641 1
+ *                 means
+ *                      1. input 009641
+ *                      2. sex as male, or 0 as female
  *
  *        Version: 1.0
  *       Revision: none
@@ -23,31 +27,36 @@
 #include <cstdint>
 int main(int argc, char *argv[])
 {
-    if(argc == 2){
-        if(std::strlen(argv[1]) == 6){
-            for(int nIndex = 0; nIndex < 6; ++nIndex){
-                if(argv[1][nIndex] < '0' || argv[1][nIndex] > '9'){
-                    return 1;
+    if(argc == 3){
+        if((std::strlen(argv[2]) == 1) && ((argv[2][0] == '0') || (argv[2][0] == '1'))){
+            if(std::strlen(argv[1]) == 6){
+                for(int nIndex = 0; nIndex < 6; ++nIndex){
+                    if(argv[1][nIndex] < '0' || argv[1][nIndex] > '9'){
+                        return 1;
+                    }
+                }
+
+                auto nWilIndex = std::atoll(argv[1]);
+                {
+                    uint32_t nShadow    = 0;
+                    uint32_t nSex       = 0;
+                    uint32_t nDress     = 0;
+                    uint32_t nAction    = 0;
+                    uint32_t nDirection = 0;
+                    uint32_t nFrame     = 0;
+
+                    nShadow = 0;
+                    nSex    = std::atol(argv[2]);
+                    nDress  = nWilIndex / 3000;
                 }
             }
 
-            auto nRes = std::atoll(argv[1]);
-            {
-                uint32_t nShadow    = 0;
-                uint32_t nSex       = 0;
-                uint32_t nDress     = 0;
-                uint32_t nAction    = 0;
-                uint32_t nDirection = 0;
-                uint32_t nFrame     = 0;
+
+
+            std::string szFileName = argv[1];
+            if(szFileName.size() == 6){
+
             }
         }
-
-
-
-        std::string szFileName = argv[1];
-        if(szFileName.size() == 6){
-
-        }
+        return 1;
     }
-    return 1;
-}
