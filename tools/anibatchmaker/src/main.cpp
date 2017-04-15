86fac9d4d0d58893c294c27ff82f92ed0263efd0
anhong <anhonghe@gmail.com>
2017-04-14 18:29:24 -0700

    start the batch maker

---
+++ tools/anibatchmaker/src/main.cpp
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename: main.cpp
+ *        Created: 04/14/2017 18:27:34
+ *  Last Modified: 04/14/2017 18:28:52
+ *
+ *    Description: translate all animations from *.wil
+ *                 shadow strategy
+ *
+ *        Version: 1.0
+ *       Revision: none
+ *       Compiler: gcc
+ *
+ *         Author: ANHONG
+ *          Email: anhonghe@gmail.com
+ *   Organization: USTC
+ *
+ * =====================================================================================
+ */
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
