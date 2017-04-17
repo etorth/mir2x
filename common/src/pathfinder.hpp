c2e1e4e9d0073a1087ca0908f275e840def71ac0
anhong <anhonghe@gmail.com>
2017-04-17 11:18:08 -0700

    back roll to use the previous path finding method

--- common/src/pathfinder.hpp
+++ common/src/pathfinder.hpp
@@ -3,9 +3,16 @@
  *
  *       Filename: pathfinder.hpp
  *        Created: 03/28/2017 17:04:54
- *  Last Modified: 04/17/2017 01:04:14
+ *  Last Modified: 04/17/2017 11:14:37
  *
  *    Description: A-Star algorithm for path find
+ *
+ *                 previously I put a (dstX, dstY) in AStarPathFinder to make each
+ *                 time when search next node, it prefers the one with the right
+ *                 direction, but this seems won't help
+ *
+ *                 if we have a prefperence, we should make it by the cost function,
+ *                 rather than hack the source to make a priority
  *
  *        Version: 1.0
  *       Revision: none
@@ -35,22 +42,11 @@ class AStarPathFinder: public AStarSearch<AStarPathFinderNode>
         std::function<bool(int, int, int, int)> m_MoveChecker;
         std::function<int (int, int, int, int)> m_MoveCost;
 
-    private:
-        // actually we can still find the path without these dst info
-        // but this dst info can give us a smooth result
-        //
-        // for each step, we try direction (currX, currY) -> (dstX, dstY) first
-        // rather than always up->right->down->left in GetSuccessors()
-        int m_FindPathDstX;
-        int m_FindPathDstY;
-
     public:
         AStarPathFinder(std::function<bool(int, int, int, int)> fnMoveChecker)
             : AStarSearch<AStarPathFinderNode>()
             , m_MoveChecker(fnMoveChecker)
             , m_MoveCost()
-            , m_FindPathDstX(-1)
-            , m_FindPathDstY(-1)
         {
             assert(fnMoveChecker);
         }
@@ -117,46 +113,12 @@ class AStarPathFinderNode
 
         bool GetSuccessors(AStarSearch<AStarPathFinderNode> *pAStarSearch, AStarPathFinderNode *pParentNode)
         {
-            // use internal direction index as 0 ~ 7
-            //
-            //          0
-            //       7     1
-            //     6    x    2
-            //       5     3
-            //          4
-            //
-            // shouldn't have value at x
-            // but put it as 0 here to avoid branch in the loop
-            static const int nDirectionV[3][3] = {
-                {7, 0, 1},
-                {6, 0, 2},
-                {5, 4, 3},
-            };
-
-            // Direction(0 ~ 7) -> (dX, dY)
-            //                         0   1   2   3   4   5   6   7
             static const int nDX[] = { 0, +1, +1, +1,  0, -1, -1, -1};
             static const int nDY[] = {-1, -1,  0, +1, +1, +1,  0, -1};
 
-            // with different (currX, currY) -> (dstX, dstY)
-            // we put sorted direction list we should try to get smooth result
-            static const int nIndexV[8][8] = {
-                {0, 1, 7, 2, 6, 3, 5, 4}, // DX = 0, DY < 0, dst at direction 0
-                {1, 2, 0, 3, 7, 4, 6, 5}, // DX > 0, DY < 0, dst at direction 1
-                {2, 3, 1, 4, 0, 5, 7, 6}, // DX > 0, DY = 0, dst at direction 2
-                {3, 4, 2, 5, 1, 6, 0, 7}, //                                  3
-                {4, 5, 3, 6, 2, 7, 1, 0}, //                                  4
-                {5, 6, 4, 7, 3, 0, 2, 1}, //                                  5
-                {6, 7, 5, 0, 4, 1, 3, 2}, //                                  6
-                {7, 0, 6, 1, 5, 2, 4, 3}, //                                  7
-            };
-
-            int nSDX = (m_Finder->m_FindPathDstX > m_CurrX) - (m_Finder->m_FindPathDstX < m_CurrX) + 1;
-            int nSDY = (m_Finder->m_FindPathDstY > m_CurrY) - (m_Finder->m_FindPathDstY < m_CurrY) + 1;
-
             for(int nIndex = 0; nIndex < 8; ++nIndex){
-                int nNewX = m_CurrX + nDX[nIndexV[nDirectionV[nSDY][nSDX]][nIndex]];
-                int nNewY = m_CurrY + nDY[nIndexV[nDirectionV[nSDY][nSDX]][nIndex]];
+                int nNewX = m_CurrX + nDX[nIndex];
+                int nNewY = m_CurrY + nDY[nIndex];
 
                 if(true
                         && pParentNode
