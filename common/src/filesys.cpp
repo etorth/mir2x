049003f4b997014c16af4f93811da508e7781e37
etorth <anhonghe@gmail.com>
2019-01-28 01:17:34 +0000

    prepare to replace filesys with c++17 filesystem

--- common/src/filesys.cpp
+++ common/src/filesys.cpp
@@ -16,91 +16,26 @@
  * =====================================================================================
  */
 
-#ifdef _WIN32
-#define _CRT_SECURE_NO_DEPRECATE
-#include <direct.h>
-#include <io.h>
-#else
-#include<sys/stat.h>
-#include<unistd.h>
-#endif
-
 #include <string>
-#include <cstdio>
 #include <cstdint>
 #include <cstdlib>
 #include <cstring>
-#include <dirent.h>
-
+#include <filesystem>
 #include "filesys.hpp"
 
 bool FileSys::RemoveDir(const char *szAbsolutePath)
 {
-    std::string szPathName = szAbsolutePath;
-    if(szPathName.back() != '/'){
-        szPathName += "/";
-    }
-
-    auto stDir = opendir(szPathName.c_str());
-    if(stDir == nullptr){
-        return false;
-    }
-
-    struct dirent *stDirItem = nullptr;
-    while((stDirItem = readdir(stDir))!= nullptr){
-        if(!std::strcmp(stDirItem->d_name, ".")){
-            continue;
-        }
-        if(!std::strcmp(stDirItem->d_name, "..")){
-            continue;
-        }
-
-        if(stDirItem->d_type == DT_DIR){
-            if(!RemoveDir((szPathName + stDirItem->d_name).c_str())){
-                return false;
-            }else{
-                rewinddir(stDir);
-                continue;
-            }
-        }
-
-        // not std::remove(), they are different functions
-        if(remove((szPathName + stDirItem->d_name).c_str()) != 0){
-            return false;
-		}else{
-			rewinddir(stDir);
-			continue;
-		}
-    }
-    return !closedir(stDir) &&
-#ifdef _WIN32
-        !_rmdir
-#else
-        !rmdir
-#endif
-        (szPathName.c_str());
-
+    return std::filesystem::remove_all(szAbsolutePath);
 }
 
 bool FileSys::MakeDir(const char *szDirName)
 {
-    return 
-#ifdef _WIN32
-        !_mkdir(szDirName);
-#else
-        !mkdir(szDirName, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
-#endif
+    return std::filesystem::create_directory(szDirName);
 }
 
 bool FileSys::FileExist(const char *szFileName)
 {
-    return 
-#ifdef _WIN32
-        !_access(szFileName, 0)
-#else
-        !access(szFileName, F_OK)
-#endif
-        ;
+    return std::filesystem::exists(szFileName);
 }
 
 bool FileSys::DupFile(const char *szDst, const char *szSrc)
