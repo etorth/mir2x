e1e8e3a2e4daf947370ac8f165fe5dd625a2ce50
etorth <anhonghe@gmail.com>
2019-01-29 09:10:42 +0000

    caught all possible exception for client main

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -15,6 +15,8 @@
  *
  * =====================================================================================
  */
+
+#include <cstdio>
 #include "log.hpp"
 #include "client.hpp"
 #include "xmlconf.hpp"
@@ -50,49 +52,59 @@ Client          *g_Client          = nullptr; // gobal instance
 int main(int argc, char *argv[])
 {
     std::srand((unsigned int)std::time(nullptr));
-    arg_parser stCmdParser(argc, argv);
+    try{
+        arg_parser stCmdParser(argc, argv);
+        auto fnAtExit = []()
+        {
+            delete g_ClientArgParser; g_ClientArgParser = nullptr;
+            delete g_Log            ; g_Log             = nullptr;
+            delete g_XMLConf        ; g_XMLConf         = nullptr;
+            delete g_SDLDevice      ; g_SDLDevice       = nullptr;
+            delete g_ProgUseDBN     ; g_ProgUseDBN      = nullptr;
+            delete g_GroundItemDBN  ; g_GroundItemDBN   = nullptr;
+            delete g_CommonItemDBN  ; g_CommonItemDBN   = nullptr;
+            delete g_MapDBN         ; g_MapDBN          = nullptr;
+            delete g_HeroDBN        ; g_HeroDBN         = nullptr;
+            delete g_MonsterDBN     ; g_MonsterDBN      = nullptr;
+            delete g_FontexDBN      ; g_FontexDBN       = nullptr;
+            delete g_MapBinDBN      ; g_MapBinDBN       = nullptr;
+            delete g_EmoticonDBN    ; g_EmoticonDBN     = nullptr;
+            delete g_NotifyBoard    ; g_NotifyBoard     = nullptr;
+            delete g_Client         ; g_Client          = nullptr;
+        };
 
-    auto fnAtExit = []()
-    {
-        delete g_ClientArgParser; g_ClientArgParser = nullptr;
-        delete g_Log            ; g_Log             = nullptr;
-        delete g_XMLConf        ; g_XMLConf         = nullptr;
-        delete g_SDLDevice      ; g_SDLDevice       = nullptr;
-        delete g_ProgUseDBN     ; g_ProgUseDBN      = nullptr;
-        delete g_GroundItemDBN  ; g_GroundItemDBN   = nullptr;
-        delete g_CommonItemDBN  ; g_CommonItemDBN   = nullptr;
-        delete g_MapDBN         ; g_MapDBN          = nullptr;
-        delete g_HeroDBN        ; g_HeroDBN         = nullptr;
-        delete g_MonsterDBN     ; g_MonsterDBN      = nullptr;
-        delete g_FontexDBN      ; g_FontexDBN       = nullptr;
-        delete g_MapBinDBN      ; g_MapBinDBN       = nullptr;
-        delete g_EmoticonDBN    ; g_EmoticonDBN     = nullptr;
-        delete g_NotifyBoard    ; g_NotifyBoard     = nullptr;
-        delete g_Client         ; g_Client            = nullptr;
-    };
+        std::atexit(fnAtExit);
 
-    std::atexit(fnAtExit);
+        g_ClientArgParser = new ClientArgParser(stCmdParser);
+        g_Log             = new Log("mir2x-client-v0.1");
 
-    g_ClientArgParser = new ClientArgParser(stCmdParser);
-    g_Log             = new Log("mir2x-client-v0.1");
-    g_XMLConf         = new XMLConf();
-    g_SDLDevice       = new SDLDevice();
-    g_ProgUseDBN      = new PNGTexDBN();
-    g_GroundItemDBN   = new PNGTexDBN();
-    g_CommonItemDBN   = new PNGTexDBN();
-    g_MapDBN          = new PNGTexDBN();
-    g_HeroDBN         = new PNGTexOffDBN();
-    g_MonsterDBN      = new PNGTexOffDBN();
-    g_WeaponDBN       = new PNGTexOffDBN();
-    g_MagicDBN        = new PNGTexOffDBN();
-    g_FontexDBN       = new FontexDBN();
-    g_MapBinDBN       = new MapBinDBN();
-    g_EmoticonDBN     = new EmoticonDBN();
-    g_Client          = new Client();       // loads fontex resource
-    g_NotifyBoard     = new NotifyBoard();  // needs fontex
+    }catch(const std::exception &e){
+        std::fprintf(stderr, "Caught exception: %s\n", e.what());
+        return -1;
+    }catch(...){
+        std::fprintf(stderr, "Caught unknown exception, exit...\n");
+        return -1;
+    }
 
     try{
+        g_XMLConf         = new XMLConf();
+        g_SDLDevice       = new SDLDevice();
+        g_ProgUseDBN      = new PNGTexDBN();
+        g_GroundItemDBN   = new PNGTexDBN();
+        g_CommonItemDBN   = new PNGTexDBN();
+        g_MapDBN          = new PNGTexDBN();
+        g_HeroDBN         = new PNGTexOffDBN();
+        g_MonsterDBN      = new PNGTexOffDBN();
+        g_WeaponDBN       = new PNGTexOffDBN();
+        g_MagicDBN        = new PNGTexOffDBN();
+        g_FontexDBN       = new FontexDBN();
+        g_MapBinDBN       = new MapBinDBN();
+        g_EmoticonDBN     = new EmoticonDBN();
+        g_Client          = new Client();       // loads fontex resource
+        g_NotifyBoard     = new NotifyBoard();  // needs fontex
+
         g_Client->MainLoop();
+
     }catch(const std::exception &e){
         g_Log->AddLog(LOGTYPE_FATAL, "Caught exception: %s", e.what());
     }catch(...){
