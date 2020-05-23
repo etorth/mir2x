3e3029f6e1cc8e14b60be6e82c841c987199b1d0
etorth <anhonghe@gmail.com>
2020-05-23 01:28:38 +0000

    fix compilation issue

--- client/src/texaniboard.cpp
+++ client/src/texaniboard.cpp
@@ -15,16 +15,17 @@
  * =====================================================================================
  */
 
+#include <numeric>
 #include "pngtexdb.hpp"
 #include "sdldevice.hpp"
-#include "texanimation.hpp"
+#include "texaniboard.hpp"
 
 extern PNGTexDB *g_progUseDB;
 extern SDLDevice *g_SDLDevice;
 
 TexAniBoard::TexAniBoard(int x, int y, uint32_t texID, size_t frameCount, size_t fps, bool fadeInout, bool loop, Widget *pwidget, bool autoDelete)
     : Widget(x, y, 0, 0, pwidget, autoDelete)
-    : m_fps(fps)
+    , m_fps(fps)
     , m_accuTime(0.0)
     , m_loop(loop)
     , m_fadeInout(fadeInout)
@@ -41,7 +42,7 @@ void TexAniBoard::update(double fUpdateTime)
 void TexAniBoard::drawEx(int dstX, int dstY, int, int, int, int)
 {
     const double frameTime = 1000.0 / m_fps;
-    const auto frame = [this]() -> int
+    const auto frame = [frameTime, this]() -> int
     {
         const auto frame = std::lround(std::floor(m_accuTime / frameTime));
         if(m_loop){
@@ -50,7 +51,7 @@ void TexAniBoard::drawEx(int dstX, int dstY, int, int, int, int)
         return std::min<int>(frame, m_texSeq.size() - 1);
     }();
 
-    const auto alpha = [this]() -> int
+    const auto alpha = [frameTime, frame, this]() -> int
     {
         if(m_fadeInout){
             return (int)(std::lround(255 * std::max<double>(0.0, m_accuTime / frameTime - frame)));
