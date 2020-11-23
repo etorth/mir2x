95862ddd0d668f3492e7da0a3081d05a3fa5c70f
etorth <anhonghe@gmail.com>
2020-11-22 20:50:01 -0800

    f

---
+++ server/monoserver/script/npc/道馆.铁匠.lua
@@ -0,0 +1,18 @@
+-- =====================================================================================
+--
+--       Filename: 道馆.铁匠.lua
+--        Created: 11/22/2020 08:52:57 PM
+--    Description: lua 5.3
+--
+--        Version: 1.0
+--       Revision: none
+--       Compiler: lua
+--
+--         Author: ANHONG
+--          Email: anhonghe@gmail.com
+--   Organization: USTC
+--
+-- =====================================================================================
+
+-- NPC script
+-- provides the table: processNPCEvent for event processing
