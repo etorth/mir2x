fdf1e72b05d4ab3cc1b12dc76eb07fdd82bc6293
etorth <anhonghe@gmail.com>
2021-03-13 23:26:44 -0800

    addCWLog

--- server/monoserver/src/monoserver.lua
+++ server/monoserver/src/monoserver.lua
@@ -1,5 +1,13 @@
 --, u8R"###(
 --
+
+function addCWLog(logType, logPrompt, logString, ...)
+    if type(logType) ~= 'number' or type(logPrompt) ~= 'string' or type(logString) ~= 'string' then
+        error(string.format('invalid argument type: addCWLog(%s, %s, %s, ...)', type(logType), type(logPrompt), type(logString)))
+    end
+    addCWLogString(logType, logPrompt, logString:format(...))
+end
+
 function listMap()
     local mapNameTable = {}
     for k, v in ipairs(getMapIDList()) do
