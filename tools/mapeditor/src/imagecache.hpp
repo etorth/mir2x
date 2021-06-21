74c33625cedd8bb57e27b8ab9e06b8b3dde8e8fb
etorth <anhonghe@gmail.com>
2021-06-20 21:12:08 -0700

    f

--- tools/mapeditor/src/imagecache.hpp
+++ tools/mapeditor/src/imagecache.hpp
@@ -17,6 +17,8 @@
  */
 
 #pragma once
+#include <cstdint>
+#include <vector>
 #include <memory>
 #include <unordered_map>
 #include <FL/Fl_RGB_Image.H>
@@ -26,6 +28,7 @@
 class ImageCache
 {
     private:
+        std::vector<uint32_t> m_convBuf;
         std::unordered_map<uint32_t, std::unique_ptr<Fl_Image>> m_cache;
 
     public:
@@ -38,5 +41,11 @@ class ImageCache
         Fl_Image *retrieve(uint8_t fileIndex, uint16_t imageIndex)
         {
             return retrieve((to_u32(fileIndex) << 16) + imageIndex);
+        }
+
+    public:
+        void clear()
+        {
+            m_cache.clear();
         }
 };
--- tools/mapwil2png/src/main.cpp
+++ tools/mapwil2png/src/main.cpp
@@ -18,6 +18,7 @@
  * =====================================================================================
  */
 
+#include <vector>
 #include <string>
 #include <cstdlib>
 #include <cstdint>
@@ -25,6 +26,7 @@
 
 #include "strf.hpp"
 #include "imgf.hpp"
+#include "alphaf.hpp"
 #include "imagedb.hpp"
 #include "fflerror.hpp"
 
@@ -38,14 +40,22 @@ int main(int argc, char *argv[])
     fflassert(str_haschar(argv[2])); // map-png-output-path
 
     std::string fileName;
-    ImageDB(argv[1]).extract([argv, &fileName](uint8_t fileIndex, uint16_t imageIndex, const void *imgBuf, size_t imgW, size_t imgH)
+    std::vector<uint32_t> convBuf;
+    ImageDB(argv[1]).extract([argv, &fileName, &convBuf](uint8_t fileIndex, uint16_t imageIndex, const void *imgBuf, size_t imgW, size_t imgH)
     {
         fflassert(imgBuf);
         fflassert(imgW > 0);
         fflassert(imgH > 0);
 
+        const auto p    = (const uint32_t *)(imgBuf);
+        const auto pend = (const uint32_t *)(imgBuf) + imgW * imgH;
+
+        convBuf.clear();
+        convBuf.assign(p, pend);
+        alphaf::autoShadowRemove(convBuf.data(), imgW, imgH, colorf::BLACK + colorf::A_SHF(0X80));
+
         str_printf(fileName, "%s/%08llX.PNG", argv[2], (to_llu(fileIndex) << 16) + imageIndex);
-        imgf::saveImageBuffer(imgBuf, imgW, imgH, fileName.c_str());
+        imgf::saveImageBuffer(convBuf.data(), imgW, imgH, fileName.c_str());
     });
     return 0;
 }
