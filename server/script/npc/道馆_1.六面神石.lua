e5f1d5b46c507dbc32f08a6cb549990eda640355
etorth <anhonghe@gmail.com>
2021-06-29 12:31:05 -0700

    f

--- server/script/npc/道馆_1.六面神石.lua
+++ server/script/npc/道馆_1.六面神石.lua
@@ -23,7 +23,7 @@ setNPCGLoc(416, 179)
 
 local tp = require('npc.include.teleport')
 tp.setTeleport({
-    {map = '比奇县_0', x = 447, y = 386, gold = 10},
-    {map = '银杏山谷', x = 246, y = 200, gold = 25, level = 2},
-    {map = '沙巴克',   x = 216, y = 148, gold = 50, level = 3},
+    {map = '比奇县_0',    x = 447, y = 386, gold = 10},
+    {map = '沙巴克城_3',  x = 216, y = 148, gold = 50, level = 3},
+    {map = '银杏山谷_02', x = 246, y = 200, gold = 25, level = 2},
 })
