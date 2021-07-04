2f071f00f34c34452126a497a2af27876bcc3d66
etorth <anhonghe@gmail.com>
2021-07-03 22:47:46 -0700

    f

--- server/script/npc/道馆_1.王铁匠_1.lua
+++ server/script/npc/道馆_1.王铁匠_1.lua
@@ -1,2 +1,68 @@
 setNPCLook(0)
 setNPCGLoc(382, 198)
+
+processNPCEvent =
+{
+    [SYS_NPCINIT] = function(uid, value)
+        if uidQueryRedName(uid) then
+            uidPostXML(uid,
+            [[
+                <layout>
+                    <par>我不想和你这种坏人做生意。</par>
+                    <par></par>
+
+                    <par><event id="%s">关闭</event></par>
+                </layout>
+            ]], SYS_NPCDONE)
+        else
+            uidPostXML(uid,
+            [[
+                <layout>
+                    <par>欢迎光临，有什么事吗？</par>
+                    <par></par>
+
+                    <par><event id="npc_goto_1">修理</event>武器</par>
+                    <par><event id="npc_goto_2">特殊修理</event>武器</par>
+                    <par><event id="npc_goto_3">对今日的任务进行了解</event></par>
+                    <par><event id="%s">关闭</event></par>
+                </layout>
+            ]], getSubukGuildName(), SYS_NPCDONE)
+        end
+    end,
+
+    ["npc_goto_1"] = function(uid, value)
+        uidPostXML(uid,
+        [[
+            <layout>
+                <par>请把要修理的武器拿来。</par>
+                <par></par>
+
+                <par><event id="%s">前一步</event></par>
+            </layout>
+        ]], SYS_NPCINIT)
+    end,
+
+    ["npc_goto_2"] = function(uid, value)
+        uidPostXML(uid,
+        [[
+            <layout>
+                <par>我不会。</par>
+                <par></par>
+
+                <par><event id="%s">前一步</event></par>
+            </layout>
+        ]], SYS_NPCINIT)
+    end,
+
+    ["npc_goto_3"] = function(uid, value)
+        uidPostXML(uid,
+        [[
+            <layout>
+                <par>今天没事情可拜托你了。</par>
+                <par></par>
+
+                <par><event id="%s">关闭</event></par>
+            </layout>
+        ]], SYS_NPCDONE)
+    end,
+}
