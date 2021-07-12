51dc970ec28051b8e5fdba2850bfb1b96fde0a09
etorth <anhonghe@gmail.com>
2021-07-12 00:50:09 -0700

    f

--- server/script/npc/比奇县_0.六面神石_1.lua
+++ server/script/npc/比奇县_0.六面神石_1.lua
@@ -1,2 +1,8 @@
 setNPCLook(56)
 setNPCGLoc(498, 463)
+
+local tp = require('npc.include.teleport')
+tp.setTeleport('<par>我可以送你去银杏山村，你要去吗？</par>',
+{
+    {name = '银杏山村', map = '银杏山谷_02', x = 250, y = 143, gold = 500},
+})
