e0359425174fb95c1a1544479335a94870a618d6
etorth <anhonghe@gmail.com>
2021-07-11 22:02:09 -0700

    f

--- server/script/npc/include/teleport.lua
+++ server/script/npc/include/teleport.lua
@@ -55,12 +55,17 @@ end
 -- sample:
 --
 -- tp.setTeleport({
---     {map = '比奇省',   x = 447, y = 386, gold = 10},
---     {map = '银杏山谷', x = 246, y = 200, gold = 25, level = 2},
---     {map = '沙巴克',   x = 216, y = 148, gold = 50, level = 3},
+--     {name = '比奇城', map = '比奇县_0', x = 447, y = 386, gold = 10},
+--     {map = '沙巴克城_3', x = 216, y = 148, gold = 50, level = 3},
+--     {}
+--     {map = '银杏山谷_02', x = 246, y = 200, gold = 25, level = 2},
 -- })
 --
-function tp.setTeleport(dst)
+function tp.setTeleport(titlePar, dst)
+    if type(titlePar) ~= 'string' then
+        fatalPrintf('expect an XML paragraph string, get %s', type(titlePar))
+    end
+
     if processNPCEvent ~= nil then
         fatalPrintf('processNPCEvent has already been defined')
     end
@@ -73,36 +78,56 @@ function tp.setTeleport(dst)
     local processHandle = {}
 
     for i, d in ipairs(dst) do
-        if type(d.map) ~= 'string' then
-            addLog(LOGTYPE_WARNING, 'ignore invalid map: npc = %s', getNPCName())
-        elseif type(d.x) ~= 'number' or type(d.y) ~= 'number' then
-            addLog(LOGTYPE_WARNING, 'ignore invalid map location: npc = %s, map = %s', getNPCName(), d.map)
+        if type(d) ~= 'table' then
+            fatalPrintf('expect table entry, get %s', type(d))
         else
-            local gold = argDef(d.gold, 0)
-            local level = argDef(d.level, 0)
-            local gotoTag = string.format('tp_goto_%d', i)
-
-            local mapName = ''
-            local startPos = string.find(d.map, '_')
-
-            if startPos ~= nil then
-                mapName = string.sub(d.map, 1, startPos - 1)
-            else
-                mapName = d.map
+            local tableSize = 0
+            for _k, _v in pairs(d) do
+                tableSize = tableSize + 1
             end
 
-            if gold > 0 and level > 0 then
-                dstParList = dstParList .. string.format('<par><event id = "%s">%s（金币%d，等级%d）</event></par>', gotoTag, mapName, gold, level)
-            elseif gold > 0 then
-                dstParList = dstParList .. string.format('<par><event id = "%s">%s（金币%d）</event></par>', gotoTag, mapName, gold)
-            elseif level > 0 then
-                dstParList = dstParList .. string.format('<par><event id = "%s">%s（等级%d）</event></par>', gotoTag, mapName, level)
+            if tableSize == 0 then
+                dstParList = dstParList .. '<par></par>'
             else
-                dstParList = dstParList .. string.format('<par><event id = "%s">%s</event></par>', gotoTag, mapName)
-            end
+                if type(d.map) ~= 'string' then
+                    addLog(LOGTYPE_WARNING, 'ignore invalid map: npc = %s', getNPCName())
+                elseif type(d.x) ~= 'number' or type(d.y) ~= 'number' then
+                    addLog(LOGTYPE_WARNING, 'ignore invalid map location: npc = %s, map = %s', getNPCName(), d.map)
+                else
+                    local gold = argDef(d.gold, 0)
+                    local level = argDef(d.level, 0)
+                    local gotoTag = string.format('tp_goto_%d', i)
 
-            processHandle[gotoTag] = function(uid, value)
-                tp.uidReqSpaceMove(uid, d.map, d.x, d.y, gold, level)
+                    local mapName = ''
+                    if d.name ~= nil then
+                        if type(d.name) == 'string' then
+                            mapName = d.name
+                        else
+                            fatalPrintf('invalid argument: d.name has type %s', type(d.name))
+                        end
+                    else
+                        local startPos = string.find(d.map, '_')
+                        if startPos ~= nil then
+                            mapName = string.sub(d.map, 1, startPos - 1)
+                        else
+                            mapName = d.map
+                        end
+                    end
+
+                    if gold > 0 and level > 0 then
+                        dstParList = dstParList .. string.format('<par><event id = "%s">%s（金币%d，等级%d）</event></par>', gotoTag, mapName, gold, level)
+                    elseif gold > 0 then
+                        dstParList = dstParList .. string.format('<par><event id = "%s">%s（金币%d）</event></par>', gotoTag, mapName, gold)
+                    elseif level > 0 then
+                        dstParList = dstParList .. string.format('<par><event id = "%s">%s（等级%d）</event></par>', gotoTag, mapName, level)
+                    else
+                        dstParList = dstParList .. string.format('<par><event id = "%s">%s</event></par>', gotoTag, mapName)
+                    end
+
+                    processHandle[gotoTag] = function(uid, value)
+                        tp.uidReqSpaceMove(uid, d.map, d.x, d.y, gold, level)
+                    end
+                end
             end
         end
     end
@@ -115,13 +140,12 @@ function tp.setTeleport(dst)
         uidPostXML(uid,
         [[
             <layout>
-                <par>客官%s你好，我是%s，欢迎来到传奇旧时光！<emoji id="0"/></par>
-                <par>你想去哪里呢？</par>
+                %s
                 <par></par>
                 %s
                 <par><event id="%s">关闭</event></par>
             </layout>
-        ]], uidQueryName(uid), getNPCName(), dstParList, SYS_NPCDONE)
+        ]], titlePar, dstParList, SYS_NPCDONE)
     end
     processNPCEvent = processHandle
 end
--- server/script/npc/道馆_1.六面神石_1.lua
+++ server/script/npc/道馆_1.六面神石_1.lua
@@ -2,8 +2,26 @@ setNPCLook(56)
 setNPCGLoc(416, 179)
 
 local tp = require('npc.include.teleport')
-tp.setTeleport({
-    {map = '比奇县_0',    x = 447, y = 386, gold = 10},
-    {map = '沙巴克城_3',  x = 216, y = 148, gold = 50, level = 3},
-    {map = '银杏山谷_02', x = 246, y = 200, gold = 25, level = 2},
+tp.setTeleport('<par>你想去哪里？</par>',
+{
+    {name = '比奇城', map = '比奇县_0', x = 370, y = 335, gold = 500},
+    {},
+
+    {name = '毒蛇山村', map = '毒蛇山谷_2', x = 306, y = 245, gold = 1000},
+    {name = '银杏山村', map = '银杏山谷_02', x = 249, y = 145, gold = 1000},
+    {name = '边境村庄', map = '边境城市_01', x = 456, y = 217, gold = 1000},
+    {},
+
+    {name = '潘夜岛村', map = '潘夜岛_8', x = 288, y = 242, gold = 1500},
+    {name = '盟重土城', map = '盟重县_74', x = 349, y = 330, gold = 1500},
+    {},
+
+    {map = '沙漠土城_5', x = 204, y = 290, gold = 2000},
+    {},
+
+    {map = '绿洲_4', x = 435, y = 84, gold = 2500},
+    {},
+
+    {name = '攻城战地域', map = '沙巴克城_3', x = 222, y = 160, gold = 500},
+    {},
 })
