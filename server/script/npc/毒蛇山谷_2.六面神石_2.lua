f3c6833b92b13a0d4f23e9875cddd4f01579bfb5
etorth <anhonghe@gmail.com>
2021-07-13 20:32:14 -0700

    f

--- server/script/npc/毒蛇山谷_2.六面神石_2.lua
+++ server/script/npc/毒蛇山谷_2.六面神石_2.lua
@@ -1,2 +1,8 @@
 setNPCLook(56)
 setNPCGLoc(314, 193)
+
+local tp = require('npc.include.teleport')
+tp.setTeleport('<par>你想去哪里？</par>',
+{
+    {name = '攻城战地域', map = '沙巴克城_3', x = 223, y = 158, gold = 500},
+})
--- server/script/npc/盟重县_74.六面神石_2.lua
+++ server/script/npc/盟重县_74.六面神石_2.lua
@@ -1,2 +1,12 @@
 setNPCLook(56)
 setNPCGLoc(271, 267)
+
+local tp = require('npc.include.teleport')
+tp.setTeleport('<par>你想去哪里？</par>',
+{
+    {map = '沙漠土城_5', x = 205, y = 288, gold = 500},
+    {name = '石阁庙地下城入口', map = '石阁庙_75', x = 185, y = 89, gold = 500},
+    {},
+
+    {map = '绿洲_4', x = 436, y = 82, gold = 1000},
+})
