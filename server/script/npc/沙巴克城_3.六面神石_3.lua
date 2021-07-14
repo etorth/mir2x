7d55266711f7d27c3b1a729002fa9e95cc8bf834
etorth <anhonghe@gmail.com>
2021-07-13 20:54:28 -0700

    f

--- server/script/npc/沙巴克城_3.六面神石_3.lua
+++ server/script/npc/沙巴克城_3.六面神石_3.lua
@@ -1,2 +1,9 @@
 setNPCLook(56)
 setNPCGLoc(49, 566)
+
+local tp = require('npc.include.teleport')
+tp.setTeleport('<par>你想去哪里？</par>',
+{
+    {name = '道馆村', map = '道馆_1', x = 417, y = 178, gold = 500},
+    {name = '毒蛇山村', map = '毒蛇山谷_2', x = 315, y = 192, gold = 500},
+})
