b38b76db5e6874155a4eb0d6eb904ae54df8eea4
etorth <anhonghe@gmail.com>
2021-07-17 02:32:16 -0700

    f

--- server/script/map/厨房_D90222.lua
+++ server/script/map/厨房_D90222.lua
@@ -4,7 +4,7 @@ local addMonCo = addmon.monGener( -- 厨房_D90222
     {
         name = '触角神魔',
         loc = {
-            {x = 25, y = 25, w = 25, h = 25, count = 1, time = 1200, cratio = 0},
+            {x = 3, y = 15, w = 25, h = 25, count = 1, time = 1200, cratio = 0},
         }
     },
 })
