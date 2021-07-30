c0fdc5c3c61745d587e8f83db1d6ebce57229d0e
etorth <anhonghe@gmail.com>
2021-07-29 18:35:47 -0700

    f

--- client/src/clientcavemaggot.hpp
+++ client/src/clientcavemaggot.hpp
@@ -24,7 +24,7 @@ class ClientCaveMaggot: public ClientMonster
 
             m_motionQueue.back()->addUpdate(false, [this](MotionNode *motionPtr) -> bool
             {
-                if(motionPtr->frame < 5){
+                if(motionPtr->frame < 3){
                     return false;
                 }
 
