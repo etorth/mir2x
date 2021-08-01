7bd0f26f03da4c9942e89d6474fa280f3019f3ce
etorth <anhonghe@gmail.com>
2021-07-31 23:19:53 -0700

    f

--- server/src/serverbombspider.hpp
+++ server/src/serverbombspider.hpp
@@ -2,11 +2,11 @@
 #include "dbcomid.hpp"
 #include "monster.hpp"
 
-class SandCactus final: public Monster
+class ServerBombSpider final: public Monster
 {
     public:
-        SandCactus(ServerMap *mapPtr, int argX, int argY)
-            : Monster(DBCOM_MONSTERID(u8"沙漠树魔"), mapPtr, argX, argY, DIR_BEGIN, 0)
+        ServerBombSpider(ServerMap *mapPtr, int argX, int argY, int argDir)
+            : Monster(DBCOM_MONSTERID(u8"爆裂蜘蛛"), mapPtr, argX, argY, argDir, 0)
         {}
 
     protected:
@@ -14,10 +14,4 @@ class SandCactus final: public Monster
 
     protected:
         DamageNode getAttackDamage(int) const override;
-
-    protected:
-        bool canMove() const override
-        {
-            return false;
-        }
 };
