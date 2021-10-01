9cf27dac4dba8cfcdf2ae6d06d42084e105873e4
etorth <anhonghe@gmail.com>
2021-09-30 20:21:47 -0700

    f

--- common/src/itemrecord.hpp
+++ common/src/itemrecord.hpp
@@ -1,35 +1,3 @@
-/*
- * =====================================================================================
- *
- *       Filename: itemrecord.hpp
- *        Created: 07/28/2017 17:12:29
- *    Description: for each item I have two GfxID:
- *                      PkgGfxID : when on ground and in inventory
- *                      UseGfxID : when drawing with player body
- *
- *                 why should I introduce all two different IDs?
- *
- *                 Best practice should be one unique GfxID for all items and retrieve
- *                 different set of animations from different archieves using the unique
- *                 GfxID.
- *
- *                 But in ghe graphical libraries, one item may share one GfxID for one
- *                 kind of usage but own its own resource for another usage. i.e. for the
- *                 诅咒屠龙 and 幸运屠龙, when drawing it with human body they are sharing
- *                 one set of animation, but when staying in inventory they have their own
- *                 different animations.
- *
- *        Version: 1.0
- *       Revision: none
- *       Compiler: gcc
- *
- *         Author: ANHONG
- *          Email: anhonghe@gmail.com
- *   Organization: USTC
- *
- * =====================================================================================
- */
-
 #pragma once
 #include <string>
 #include <cstdint>
