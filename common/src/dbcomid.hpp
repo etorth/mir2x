15fd3a9f13bd5d96ac7b772acd8803dd98835aeb
etorth <anhonghe@gmail.com>
2022-03-27 18:49:59 -0700

    O(1) DBCOM_XXXID() function

--- common/src/dbcomid.hpp
+++ common/src/dbcomid.hpp
@@ -27,6 +27,48 @@
 
 namespace
 {
+    // constexpr string hash
+    // code from: http://www.cse.yorku.ca/~oz/hash.html
+
+    // return hash code as size_t
+    // don't do careless cast to uint32_t, cast with modulo can give incorrect hash table slot
+
+    constexpr size_t u8StrViewHashHelper(const char8_t *s) noexcept
+    {
+        size_t h = 0;
+        int c;
+
+        for(auto p = s; (c = *p); ++p){
+            h = c + (h << 6U) + (h << 16U) - h;
+        }
+        return h;
+    }
+
+    template<typename T, uint32_t TL> constexpr auto recordNameOffsetHelper(const T (&itemList)[TL])
+    {
+        static_assert(TL >= 1);
+        std::array<uint32_t, 3 * TL + 127> result {};
+
+        // 1. itemOffList is much longer than itemList
+        // 2. skip itemList[0], off 0 is reserved
+        // 3, skip itemList[i], if str_haschar(itemList[i].name) is false
+
+        for(uint32_t i = 1; i < TL; ++i){
+            if(itemList[i].name && itemList[i].name[0]){
+                for(size_t j = 0, h = u8StrViewHashHelper(itemList[i].name); j < result.size(); ++j){
+                    if(const size_t c = (h + j) % result.size(); !result[c]){
+                        result[c] = i;
+                        break;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
+
+namespace
+{
     // to support use following statement in switch/case
     //
     //      switch(nMonsterID){
@@ -81,26 +123,52 @@ namespace
     };
 }
 
-template<typename T, size_t N> constexpr uint32_t DBCOM_IDHELPER(const T (&itemList)[N], const char8_t *name)
+namespace
 {
+    constexpr auto _inn_ItemRecordOffList           = recordNameOffsetHelper(_inn_ItemRecordList          );
+    constexpr auto _inn_BuffActRecordOffList        = recordNameOffsetHelper(_inn_BuffActRecordList       );
+    constexpr auto _inn_BuffRecordOffList           = recordNameOffsetHelper(_inn_BuffRecordList          );
+    constexpr auto _inn_AttackModifierRecordOffList = recordNameOffsetHelper(_inn_AttackModifierRecordList);
+    constexpr auto _inn_SpellModifierRecordOffList  = recordNameOffsetHelper(_inn_SpellModifierRecordList );
+    constexpr auto _inn_MonsterRecordOffList        = recordNameOffsetHelper(_inn_MonsterRecordList       );
+    constexpr auto _inn_MagicRecordOffList          = recordNameOffsetHelper(_inn_MagicRecordList         );
+    constexpr auto _inn_MapRecordOffList            = recordNameOffsetHelper(_inn_MapRecordList           );
+}
+
+template<typename T, uint32_t TL, size_t HL> constexpr uint32_t DBCOM_IDHELPER(const T (&itemList)[TL], const std::array<uint32_t, HL> &itemOffList, const char8_t *name)
+{
+    static_assert(TL > 0);
+    static_assert(HL > 0);
+
     if(name && name[0]){
-        for(uint32_t i = 0; i < N; ++i){
-            if(itemList[i].name && std::u8string_view(itemList[i].name) == name){
-                return i;
+        const size_t h = u8StrViewHashHelper(name);
+        for(size_t j = 0; j < HL; ++j){
+            const size_t c = (h + j) % HL;
+            const uint32_t off = itemOffList[c];
+
+            if(!off){
+                return 0;
             }
+
+            if(itemList[off].name && (std::u8string_view(itemList[off].name) == name)){
+                return off;
+            }
+
+            // current slot has non-zero off
+            // but doesn't point to the item with given name, try next slot
         }
     }
     return 0;
 }
 
-constexpr uint32_t DBCOM_ITEMID          (const char8_t *name) { return DBCOM_IDHELPER(_inn_ItemRecordList,           name); }
-constexpr uint32_t DBCOM_MONSTERID       (const char8_t *name) { return DBCOM_IDHELPER(_inn_MonsterRecordList,        name); }
-constexpr uint32_t DBCOM_MAGICID         (const char8_t *name) { return DBCOM_IDHELPER(_inn_MagicRecordList,          name); }
-constexpr uint32_t DBCOM_MAPID           (const char8_t *name) { return DBCOM_IDHELPER(_inn_MapRecordList,            name); }
-constexpr uint32_t DBCOM_BUFFID          (const char8_t *name) { return DBCOM_IDHELPER(_inn_BuffRecordList,           name); }
-constexpr uint32_t DBCOM_BUFFACTID       (const char8_t *name) { return DBCOM_IDHELPER(_inn_BuffActRecordList,        name); }
-constexpr uint32_t DBCOM_ATTACKMODIFIERID(const char8_t *name) { return DBCOM_IDHELPER(_inn_AttackModifierRecordList, name); }
-constexpr uint32_t DBCOM_SPELLMODIFIERID (const char8_t *name) { return DBCOM_IDHELPER(_inn_SpellModifierRecordList,  name); }
+constexpr uint32_t DBCOM_ITEMID          (const char8_t *name) { return DBCOM_IDHELPER(_inn_ItemRecordList,           _inn_ItemRecordOffList,           name); }
+constexpr uint32_t DBCOM_MONSTERID       (const char8_t *name) { return DBCOM_IDHELPER(_inn_MonsterRecordList,        _inn_MonsterRecordOffList,        name); }
+constexpr uint32_t DBCOM_MAGICID         (const char8_t *name) { return DBCOM_IDHELPER(_inn_MagicRecordList,          _inn_MagicRecordOffList,          name); }
+constexpr uint32_t DBCOM_MAPID           (const char8_t *name) { return DBCOM_IDHELPER(_inn_MapRecordList,            _inn_MapRecordOffList,            name); }
+constexpr uint32_t DBCOM_BUFFID          (const char8_t *name) { return DBCOM_IDHELPER(_inn_BuffRecordList,           _inn_BuffRecordOffList,           name); }
+constexpr uint32_t DBCOM_BUFFACTID       (const char8_t *name) { return DBCOM_IDHELPER(_inn_BuffActRecordList,        _inn_BuffActRecordOffList,        name); }
+constexpr uint32_t DBCOM_ATTACKMODIFIERID(const char8_t *name) { return DBCOM_IDHELPER(_inn_AttackModifierRecordList, _inn_AttackModifierRecordOffList, name); }
+constexpr uint32_t DBCOM_SPELLMODIFIERID (const char8_t *name) { return DBCOM_IDHELPER(_inn_SpellModifierRecordList,  _inn_SpellModifierRecordOffList,  name); }
 
 constexpr uint32_t DBCOM_ITEMENDID          () { return std::extent_v<decltype(_inn_ItemRecordList          )>; }
 constexpr uint32_t DBCOM_MONSTERENDID       () { return std::extent_v<decltype(_inn_MonsterRecordList       )>; }
