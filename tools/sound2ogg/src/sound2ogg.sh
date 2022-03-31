bfb4bf8fb4fb5767fde175737603df63a7b4ef88
anhong <anhonghe@gmail.com>
2022-03-31 14:17:54 -0700

    f

--- tools/sound2ogg/src/sound2ogg.sh
+++ tools/sound2ogg/src/sound2ogg.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-if test $# -ne 1
+if test $# -ne 2
 then
-    echo "Usage: sound2ogg.sh <input-dir>"
+    echo "Usage: sound2ogg.sh <input-dir> <output-dir>"
     exit 1
 fi
 
 if [ ! -d $1 ]
 then
-    echo "Not a directory: $1"
+    echo "Invalid input directory: $1"
+    exit 1
+fi
+
+if [ ! -d $2 ]
+then
+    echo "Invalid output directory: $2"
     exit 1
 fi
 
@@ -23,12 +29,35 @@ fi
 # dir2ogg doesn't provide a output directory parameter
 # all output will be in input directory, this may overwrite existing .ogg files
 
+# prevent the wildcard file name itself becomes a match
+# be careful of using this
+shopt -s nullglob
+
+for oggfile in $1/*.[Oo][Gg][Gg]
+do
+    echo "WARNING: detected .ogg file in input directory: $oggfile"
+    hasOGGFile=1
+done
+
+if [ ! -z $hasOGGFile ]
+then
+    exit 1
+fi
+
 for wavfile in $1/*.[Ww][Aa][Vv]
 do
-    dir2ogg $wavfile -q 10 # use best quality
+    dir2ogg $wavfile -q 10 -Q # use best quality
 done
 
 for mp3file in $1/*.[Mm][Pp]3
 do
-    dir2ogg $mp3file -t # use original mp3 quality
+    dir2ogg $mp3file -t -Q # use original mp3 quality
+done
+
+# done conversion
+# move all .ogg files to output directory
+
+for oggfile in $1/*.[Oo][Gg][Gg]
+do
+    mv -f $oggfile $2
 done
