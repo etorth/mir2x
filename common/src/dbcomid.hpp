486a10cdac1e2fca0554036af91867ad00635c1f
etorth <anhonghe@gmail.com>
2022-04-09 02:07:59 -0700

    simplify magicrecord.hpp

--- common/src/dbcomid.hpp
+++ common/src/dbcomid.hpp
@@ -243,7 +243,7 @@ constexpr const std::optional<uint32_t> DBCOM_MAGICGFXSEFFID(const std::u8string
 
     if(const auto stageVal = magicStageID(stage.data()); (stageVal >= MST_BEGIN) && (stageVal < MST_END)){
         if(const auto [gfxEntry, gfxEntryRef] = DBCOM_MAGICGFXENTRY(name, stage); gfxEntry){
-            if(gfxEntry->seff.ref){
+            if(!gfxEntry->seff.ref.name.empty()){
                 return DBCOM_MAGICGFXSEFFID(gfxEntry->seff.ref.name, gfxEntry->seff.ref.stage.empty() ? stage : gfxEntry->seff.ref.stage);
             }
 
