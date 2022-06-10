6cc82eabd2e529b34e7651a82abfc8369a37c780
etorth <anhonghe@gmail.com>
2022-06-10 01:13:58 -0700

    f

--- server/src/player.lua
+++ server/src/player.lua
@@ -13,17 +13,39 @@ function spaceMove(mapID, x, y)
     local done = nil
     local function onOK()
         done = true
+        -- after this line
+        -- C level will call resumeCORunner(runSeqID) cooperatively
+        -- this is black magic, we can not put resumeCORunner(runSeqID) explicitly here in lua, see comments below:
+
+        -- for callback function onOK and onError
+        -- they should keep simple as above, only setup marks/flags
+
+        -- don't call other complext functions which gives callback hell
+        -- I design the player runner in sequential manner
+
+        -- more importantly, don't do runner-resume in the onOK/onError callback
+        -- which causes crash, because if we resume in onOK/onError, then when the callback gets triggeerred, stack is:
+        --
+        --   --C-->onOK/onError-->resumeCORunner(getTLSTable().runSeqID)-->code in this runner after requestSpaceMove/coroutine.yield()
+        --     ^       ^                ^                                   ^
+        --     |       |                |                                   |
+        --     |       |                |                                   +------ lua
+        --     |       |                +------------------------------------------ C
+        --     |       +----------------------------------------------------------- lua
+        --     +------------------------------------------------------------------- C
+        --
+        -- see here C->lua->C->lua with yield/resume
+        -- this crashes
     end
 
     local function onError()
         done = false
+        -- transparent logic same as onOK
     end
 
     requestSpaceMove(mapID, x, y, onOK, onError, getTLSTable().runSeqID)
 
-    while done == nil do
-        coroutine.yield()
-    end
+    coroutine.yield()
     return done
 end
 
