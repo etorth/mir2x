b0f651bf5e7d116d447b56e38776a27fcb7d1ccc
etorth <anhonghe@gmail.com>
2022-06-15 14:54:32 -0700

    SDItem::build_EA_COLOR() and SDItem::EA_COLOR_t

--- common/src/serdesmsg.hpp
+++ common/src/serdesmsg.hpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <numeric>
 #include <variant>
+#include <utility>
 #include <optional>
 #include <algorithm>
 #include <unordered_set>
@@ -159,65 +160,88 @@ struct SDItem
     constexpr static int EA_NONE  = 0;
     constexpr static int EA_BEGIN = 1;
 
-    constexpr static int _ea_def_counter_begin = __COUNTER__;
-#define _macro_def_EA_type(eaType, eaValType) constexpr static int eaType = __COUNTER__ - _ea_def_counter_begin; struct eaType##_t { constexpr static int value = eaType; using type = eaValType; };
+    constexpr static int _ea_add_type_counter_begin = __COUNTER__;
+#define _MACRO_ADD_EA_TYPE(eaType, eaValType) \
+    constexpr static int eaType = __COUNTER__ - _ea_add_type_counter_begin; \
+    struct eaType##_t \
+    { \
+        constexpr static int value = eaType; \
+        using type = eaValType; \
+    }; \
+    template<typename ... Args> static std::pair<int, std::string> build_##eaType(Args && ... args) \
+    { \
+        return {eaType, cerealf::serialize<eaValType>(eaValType(std::forward<Args>(args)...), -1)}; \
+    } \
 
-    _macro_def_EA_type(EA_DC , int)
-    _macro_def_EA_type(EA_MC , int)
-    _macro_def_EA_type(EA_SC , int)
-    _macro_def_EA_type(EA_AC , int)
-    _macro_def_EA_type(EA_MAC, int)
+    /**/ // begin of extra-attributes
+    /**/ // each line provides three types, take EA_DC as example:
+    /**/ //
+    /**/ //     SDItem::EA_DC           : an integer used as key
+    /**/ //     SDItem::EA_DC_t         : an type contains: EA_DC_t::value and EA_DC_t::type
+    /**/ //     SDItem::build_EA_DC()   : an function returns std::pair<int, std::string> where second is the serialized extra-attributes value
+    /**/ //
+    /**/ // don't put any other code except the macro defines and type aligns
+    /**/
+    /**/ _MACRO_ADD_EA_TYPE(EA_DC , int)
+    /**/ _MACRO_ADD_EA_TYPE(EA_MC , int)
+    /**/ _MACRO_ADD_EA_TYPE(EA_SC , int)
+    /**/ _MACRO_ADD_EA_TYPE(EA_AC , int)
+    /**/ _MACRO_ADD_EA_TYPE(EA_MAC, int)
+    /**/
+    /**/ _MACRO_ADD_EA_TYPE(EA_DCHIT, int)
+    /**/ _MACRO_ADD_EA_TYPE(EA_MCHIT, int)
+    /**/
+    /**/ _MACRO_ADD_EA_TYPE(EA_DCDODGE, int)
+    /**/ _MACRO_ADD_EA_TYPE(EA_MCDODGE, int)
+    /**/
+    /**/ _MACRO_ADD_EA_TYPE(EA_SPEED, int)
+    /**/ _MACRO_ADD_EA_TYPE(EA_COMFORT, int)
+    /**/ _MACRO_ADD_EA_TYPE(EA_LUCKCURSE, int)
+    /**/
+    /**/ _MACRO_ADD_EA_TYPE(EA_HPADD, int)
+    /**/ _MACRO_ADD_EA_TYPE(EA_HPSTEAL, int)
+    /**/ _MACRO_ADD_EA_TYPE(EA_HPRECOVER, int)
+    /**/
+    /**/ _MACRO_ADD_EA_TYPE(EA_MPADD, int)
+    /**/ _MACRO_ADD_EA_TYPE(EA_MPSTEAL, int)
+    /**/ _MACRO_ADD_EA_TYPE(EA_MPRECOVER, int)
+    /**/
+    /**/ _MACRO_ADD_EA_TYPE(EA_DCFIRE   , int)
+    /**/ _MACRO_ADD_EA_TYPE(EA_DCICE    , int)
+    /**/ _MACRO_ADD_EA_TYPE(EA_DCLIGHT  , int)
+    /**/ _MACRO_ADD_EA_TYPE(EA_DCWIND   , int)
+    /**/ _MACRO_ADD_EA_TYPE(EA_DCHOLY   , int)
+    /**/ _MACRO_ADD_EA_TYPE(EA_DCDARK   , int)
+    /**/ _MACRO_ADD_EA_TYPE(EA_DCPHANTOM, int)
+    /**/
+    /**/ _MACRO_ADD_EA_TYPE(EA_ACFIRE   , int)
+    /**/ _MACRO_ADD_EA_TYPE(EA_ACICE    , int)
+    /**/ _MACRO_ADD_EA_TYPE(EA_ACLIGHT  , int)
+    /**/ _MACRO_ADD_EA_TYPE(EA_ACWIND   , int)
+    /**/ _MACRO_ADD_EA_TYPE(EA_ACHOLY   , int)
+    /**/ _MACRO_ADD_EA_TYPE(EA_ACDARK   , int)
+    /**/ _MACRO_ADD_EA_TYPE(EA_ACPHANTOM, int)
+    /**/
+    /**/ _MACRO_ADD_EA_TYPE(EA_LOADBODY, int)
+    /**/ _MACRO_ADD_EA_TYPE(EA_LOADWEAPON, int)
+    /**/ _MACRO_ADD_EA_TYPE(EA_LOADINVENTORY, int)
+    /**/
+    /**/ _MACRO_ADD_EA_TYPE(EA_EXTEXP, int)
+    /**/ _MACRO_ADD_EA_TYPE(EA_EXTGOLD, int)
+    /**/ _MACRO_ADD_EA_TYPE(EA_EXTDROP, int)
+    /**/
+    /**/ _MACRO_ADD_EA_TYPE(EA_BUFFID, int)
+    /**/
+    /**/ using _helper_type_EA_TELEPORT_t = std::tuple<uint32_t, int, int>;
+    /**/ _MACRO_ADD_EA_TYPE(EA_TELEPORT, _helper_type_EA_TELEPORT_t)
+    /**/
+    /**/ _MACRO_ADD_EA_TYPE(EA_COLOR, uint32_t)
+    /**/
+    /**/ // end of extra-attributes
+    /**/ // any extra-attributes should be put inside above region
 
-    _macro_def_EA_type(EA_DCHIT, int)
-    _macro_def_EA_type(EA_MCHIT, int)
-
-    _macro_def_EA_type(EA_DCDODGE, int)
-    _macro_def_EA_type(EA_MCDODGE, int)
-
-    _macro_def_EA_type(EA_SPEED, int)
-    _macro_def_EA_type(EA_COMFORT, int)
-    _macro_def_EA_type(EA_LUCKCURSE, int)
-
-    _macro_def_EA_type(EA_HPADD, int)
-    _macro_def_EA_type(EA_HPSTEAL, int)
-    _macro_def_EA_type(EA_HPRECOVER, int)
-
-    _macro_def_EA_type(EA_MPADD, int)
-    _macro_def_EA_type(EA_MPSTEAL, int)
-    _macro_def_EA_type(EA_MPRECOVER, int)
-
-    _macro_def_EA_type(EA_DCFIRE   , int)
-    _macro_def_EA_type(EA_DCICE    , int)
-    _macro_def_EA_type(EA_DCLIGHT  , int)
-    _macro_def_EA_type(EA_DCWIND   , int)
-    _macro_def_EA_type(EA_DCHOLY   , int)
-    _macro_def_EA_type(EA_DCDARK   , int)
-    _macro_def_EA_type(EA_DCPHANTOM, int)
-
-    _macro_def_EA_type(EA_ACFIRE   , int)
-    _macro_def_EA_type(EA_ACICE    , int)
-    _macro_def_EA_type(EA_ACLIGHT  , int)
-    _macro_def_EA_type(EA_ACWIND   , int)
-    _macro_def_EA_type(EA_ACHOLY   , int)
-    _macro_def_EA_type(EA_ACDARK   , int)
-    _macro_def_EA_type(EA_ACPHANTOM, int)
-
-    _macro_def_EA_type(EA_LOADBODY, int)
-    _macro_def_EA_type(EA_LOADWEAPON, int)
-    _macro_def_EA_type(EA_LOADINVENTORY, int)
-
-    _macro_def_EA_type(EA_EXTEXP, int)
-    _macro_def_EA_type(EA_EXTGOLD, int)
-    _macro_def_EA_type(EA_EXTDROP, int)
-
-    _macro_def_EA_type(EA_BUFFID, int)
-
-    using _helper_type_EA_TELEPORT_t = std::tuple<uint32_t, int, int>;
-    _macro_def_EA_type(EA_TELEPORT, _helper_type_EA_TELEPORT_t)
-    _macro_def_EA_type(EA_COLOR, uint32_t)
-    _macro_def_EA_type(EA_END, void)
-
-#undef _macro_def_EA_type
+#undef _MACRO_ADD_EA_TYPE
+    constexpr static int EA_END = __COUNTER__ - _ea_add_type_counter_begin;
 
     uint32_t itemID = 0;
     uint32_t  seqID = 0;
