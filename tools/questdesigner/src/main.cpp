8cd5e4f8ab4f1999117fe2b01a86cf743e7132ce
etorth <anhonghe@gmail.com>
2022-07-17 12:59:56 -0700

    f

--- tools/questdesigner/src/main.cpp
+++ tools/questdesigner/src/main.cpp
@@ -2,16 +2,16 @@
 #include <string>
 #include <cstdint>
 #include <FL/Fl.H>
-#include "mainwindow.hpp"
+#include "qdmainwindow.hpp"
 
-MainWindow *g_mainWindow = nullptr;
+QD_MainWindow *g_mainWindow = nullptr;
 
 int main()
 {
     std::srand((unsigned int)(std::time(nullptr)));
     Fl::set_font(FL_HELVETICA, "文泉驿等宽正黑");
 
-    g_mainWindow = new MainWindow();
+    g_mainWindow = new QD_MainWindow();
     g_mainWindow->showAll();
 
     return Fl::run();
