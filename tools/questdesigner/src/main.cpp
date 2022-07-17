85ec042642eb705cb7904c0bed0f34ee9ce3c5c3
etorth <anhonghe@gmail.com>
2022-07-17 00:32:43 -0700

    f

--- tools/questdesigner/src/main.cpp
+++ tools/questdesigner/src/main.cpp
@@ -1,15 +1,18 @@
+#include <ctime>
 #include <string>
 #include <cstdint>
 #include <FL/Fl.H>
-
 #include "mainwindow.hpp"
 
-MainWindow                      *g_mainWindow               = nullptr;
+MainWindow *g_mainWindow = nullptr;
 
 int main()
 {
-    g_mainWindow            = new MainWindow();
+    std::srand((unsigned int)(std::time(nullptr)));
+    Fl::set_font(FL_HELVETICA, "文泉驿等宽正黑");
 
+    g_mainWindow = new MainWindow();
     g_mainWindow->showAll();
+
     return Fl::run();
 }
--- tools/questdesigner/src/qditemchoice.hpp
+++ tools/questdesigner/src/qditemchoice.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <vector>
 #include <FL/Fl.H>
 #include <Fl/Fl_Choice.H>
 
@@ -6,4 +7,7 @@ class QD_ItemChoice: public Fl_Choice
 {
     public:
         QD_ItemChoice(int, int, int, int, const char * = nullptr);
+
+    private:
+        std::vector<Fl_Menu_Item> m_menuItemList;
 };
