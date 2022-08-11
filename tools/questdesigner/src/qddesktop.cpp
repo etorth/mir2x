9f0987e3994f8502edbc854a06227dbcd2dd45c8
etorth <anhonghe@gmail.com>
2022-08-10 21:48:08 -0700

    f

--- tools/questdesigner/src/qddesktop.cpp
+++ tools/questdesigner/src/qddesktop.cpp
@@ -1,12 +1,14 @@
+#include <cstdlib>
 #include "qddesktop.hpp"
 
 QD_Desktop::QD_Desktop(int argX, int argY, int argW, int argH, const char *argLabel)
     : Fl_Scroll(argX, argY, argW, argH, argLabel)
-{}
+{
+    color(0x567b9100);
+}
 
 void QD_Desktop::draw()
 {
     Fl_Scroll::draw();
-    fl_rectf(x(), y(), w(), h(), 0x567b9100); // skyblue
     fl_draw_box(FL_DOWN_FRAME, x(), y(), w(), h(), color());
 }
