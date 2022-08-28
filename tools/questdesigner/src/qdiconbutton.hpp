d274c41a262f3c21b6312d3b1b273b2360b76dac
etorth <anhonghe@gmail.com>
2022-08-28 14:02:17 -0700

    fltk 1.4.0 support Fl_Image::scale() now

--- tools/questdesigner/src/qdiconbutton.hpp
+++ tools/questdesigner/src/qdiconbutton.hpp
@@ -8,28 +8,6 @@
 
 class QD_IconButton: public Fl_Button
 {
-    private:
-        class Inn_ScaleIcon: public Fl_Shared_Image
-        {
-            public:
-                Inn_ScaleIcon(Fl_Image *img)
-                    : Fl_Shared_Image([]() -> std::string
-                      {
-                          static std::atomic<uint64_t> s_counter {0};
-                          return std::string("inn_icon_index_") + std::to_string(s_counter++);
-                      }().c_str(),
-
-                      [img]()
-                      {
-                          fflassert(img);
-                          return img;
-                      }())
-                {}
-        };
-
-    private:
-        std::unique_ptr<Inn_ScaleIcon> m_icon;
-
     public:
         QD_IconButton(int argX, int argY, int argW, int argH, const char *argLabel = 0)
             : Fl_Button(argX, argY, argW, argH, argLabel)
@@ -38,13 +16,12 @@ class QD_IconButton: public Fl_Button
     public:
         void image(Fl_Image *img)
         {
-            // fluid calls o->image()
-            // but Fl_Widget::image() is not a virtual function
+            // fluid calls o->image(img)
+            // but be aware that Fl_Widget::image(img) is not a virtual function
 
-            m_icon = std::make_unique<Inn_ScaleIcon>(img);
-            Fl_Button::image(m_icon.get());
-            if(m_icon){
-                m_icon->scale(w(), h(), 0, 1);
+            Fl_Button::image(img);
+            if(Fl_Button::image()){
+                Fl_Button::image()->scale(w(), h(), 0, 1);
             }
         }
 
@@ -52,8 +29,8 @@ class QD_IconButton: public Fl_Button
         void resize(int argX, int argY, int argW, int argH) override
         {
             Fl_Button::resize(argX, argY, argW, argH);
-            if(m_icon){
-                m_icon->scale(argW, argH, 0, 1);
+            if(Fl_Button::image()){
+                Fl_Button::image()->scale(w(), h(), 0, 1);
             }
         }
 
@@ -61,8 +38,8 @@ class QD_IconButton: public Fl_Button
         void draw() override
         {
             Fl_Button::draw();
-            if(m_icon){
-                m_icon->draw(x(), y(), w(), h(), 0, 0);
+            if(Fl_Button::image()){
+                Fl_Button::image()->draw(x(), y(), w(), h(), 0, 0);
             }
         }
 };
