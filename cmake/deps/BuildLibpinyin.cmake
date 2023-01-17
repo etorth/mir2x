ffaefa7301944e9d278cb238bcfc884e86fb3826
etorth <anhonghe@gmail.com>
2023-01-16 20:22:58 -0800

    add libpinyin

---
+++ cmake/deps/BuildLibpinyin.cmake
@@ -0,0 +1,31 @@
+INCLUDE(ExternalProject)
+
+ExternalProject_Add(
+    libpinyin
+
+    GIT_REPOSITORY "https://github.com/etorth/libpinyin.git"
+    GIT_TAG        "main"
+
+    SOURCE_DIR "${MIR2X_3RD_PARTY_DIR}/libpinyin"
+    INSTALL_DIR "${MIR2X_3RD_PARTY_DIR}/libpinyin/build"
+
+    UPDATE_COMMAND ""
+    PATCH_COMMAND ""
+
+    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${MIR2X_3RD_PARTY_DIR}/libpinyin/build/install -DBUILD_SHARED_LIBS=OFF
+
+    LOG_BUILD 1
+    LOG_CONFIGURE 1
+    LOG_INSTALL 1
+)
+
+SET(LIBPINYIN_INCLUDE_DIRS "${MIR2X_3RD_PARTY_DIR}/libpinyin/build/install/include/libpinyin-${LIBPINYIN_VERSION}")
+IF(WIN32)
+    SET(LIBPINYIN_LIBRARIES pinyin_static)
+ELSE()
+    SET(LIBPINYIN_LIBRARIES "${CMAKE_STATIC_LIBRARY_PREFIX}pinyin${CMAKE_STATIC_LIBRARY_SUFFIX}")
+ENDIF()
+
+INCLUDE_DIRECTORIES(SYSTEM ${LIBPINYIN_INCLUDE_DIRS})
+LINK_DIRECTORIES(${MIR2X_3RD_PARTY_DIR}/libpinyin/build/install/lib)
+ADD_DEPENDENCIES(mir2x_3rds libpinyin)
