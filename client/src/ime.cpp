c3183df9263f285877b7ad5260fe9b21b1948a31
etorth <anhonghe@gmail.com>
2023-01-18 01:06:17 -0800

    IME support

--- client/src/ime.cpp
+++ client/src/ime.cpp
@@ -1,21 +1,29 @@
+#include <vector>
 #include <atomic>
 #include <thread>
+#include <cstdint>
+#include <optional>
 #include <condition_variable>
 
-#include "ime.hpp"
 #include "pinyin.h"
+#include "ime.hpp"
+#include "totype.hpp"
 #include "fflerror.hpp"
+
+// for better implementation, check
+// https://github.com/libpinyin/ibus-libpinyin.git
 
 struct _IME_Instance final
 {
     std::string input;
     std::string prefix;
-    std::string search;
 
+    std::optional<size_t> selection;
     std::vector<std::string> candidateList;
+    std::vector<std::pair<std::string, int>> stk; // (sentence, start)
 
+    bool done;
     std::thread th;
-    std::atomic<bool> done;
 
     mutable std::mutex mtx;
     mutable std::condition_variable cond;
@@ -24,7 +32,6 @@ struct _IME_Instance final
     pinyin_instance_t *instance;
 
     _IME_Instance()
-        : done {false}
     {
         context = pinyin_init("libpinyin/data", "libpinyin/data");
         fflassert(context);
@@ -39,28 +46,89 @@ struct _IME_Instance final
         instance = pinyin_alloc_instance(context);
         fflassert(instance);
 
+        done = false;
         th = std::thread([this]()
         {
             while(!done){
                 std::unique_lock<std::mutex> lock(mtx);
-                cond.wait(lock, [this]() -> bool
-                {
-                    return done;
-                });
+                cond.wait(lock);
 
-                std::swap(input, search);
-                if(search.empty()){
+                if(done){
+                    return;
+                }
+
+                if(input.empty()){
+                    prefix.clear();
+                    selection.reset();
+                    stk.clear();
+                    candidateList.clear();
+                    pinyin_reset(instance);
                     continue;
                 }
 
-                pinyin_guess_sentence(instance);
+                if(selection.has_value()){
+                    guint num = 0;
+                    pinyin_get_n_candidate(instance, &num);
+
+                    if(selection.value() >= num){
+                        selection.reset();
+                        continue;
+                    }
+
+                    const auto choice = selection.value();
+                    selection.reset();
+
+                    lookup_candidate_t *candidate = nullptr;
+                    pinyin_get_candidate(instance, choice, &candidate);
+
+                    const char *word = nullptr;
+                    pinyin_get_candidate_string(instance, candidate, &word);
+
+                    lookup_candidate_type_t type;
+                    pinyin_get_candidate_type(instance, candidate, &type);
+
+                    const auto [sentence, offset] = [type, word, this]() -> std::pair<std::string, size_t>
+                    {
+                        if((type == NBEST_MATCH_CANDIDATE) || stk.empty()){
+                            return {word, 0};
+                        }
+                        else{
+                            return {stk.back().first + word, to_uz(stk.back().second)};
+                        }
+                    }();
+
+                    stk.emplace_back(sentence, pinyin_choose_candidate(instance, offset, candidate));
+                }
+                else if(stk.empty()){
+                    pinyin_parse_more_full_pinyins(instance, input.c_str());
+                    pinyin_guess_sentence_with_prefix(instance, prefix.c_str());
+                }
+
+                candidateList.clear();
+                pinyin_guess_candidates(instance, stk.empty() ? 0 : stk.back().second, SORT_BY_PHRASE_LENGTH_AND_PINYIN_LENGTH_AND_FREQUENCY);
+
+                guint num = 0;
+                pinyin_get_n_candidate(instance, &num);
+
+                for(guint i = 0; i < num; ++i){
+                    lookup_candidate_t *candidate = nullptr;
+                    pinyin_get_candidate(instance, i, &candidate);
+
+                    const char *word = nullptr;
+                    pinyin_get_candidate_string(instance, candidate, &word);
+
+                    candidateList.emplace_back(word);
+                }
             }
         });
     }
 
     ~_IME_Instance()
     {
-        done = true;
+        {
+            const std::lock_guard<std::mutex> lock(mtx);
+            done = true;
+        }
         cond.notify_one();
 
         th.join();
@@ -70,38 +138,6 @@ struct _IME_Instance final
 
         pinyin_save(context);
         pinyin_fini(context);
-    }
-
-
-    void feed(char ch)
-    {
-        {
-            const std::lock_guard<std::mutex> lock(mtx);
-            input.push_back(ch);
-        }
-        cond.notify_one();
-    }
-
-    void clear()
-    {
-        {
-            const std::lock_guard<std::mutex> lock(mtx);
-            input.clear();
-            candidateList.clear();
-        }
-        cond.notify_one();
-    }
-
-    std::vector<std::string> getCandidateList() const
-    {
-        const std::lock_guard<std::mutex> lock(mtx);
-        return candidateList;
-    }
-
-    std::string getInputString() const
-    {
-        const std::lock_guard<std::mutex> lock(mtx);
-        return input;
     }
 };
 
@@ -117,4 +153,101 @@ IME::IME()
 IME::~IME()
 {
     delete asIMEPtr(m_instance);
+}
+
+void IME::clear()
+{
+    const auto imePtr = asIMEPtr(m_instance);
+    {
+        const std::lock_guard<std::mutex> lock(imePtr->mtx);
+        imePtr->input.clear();
+    }
+    imePtr->cond.notify_one();
+}
+
+void IME::feed(char ch)
+{
+    const auto imePtr = asIMEPtr(m_instance);
+    {
+        const std::lock_guard<std::mutex> lock(imePtr->mtx);
+        imePtr->input.push_back(ch);
+    }
+    imePtr->cond.notify_one();
+}
+
+void IME::backspace()
+{
+    const auto imePtr = asIMEPtr(m_instance);
+    {
+        const std::lock_guard<std::mutex> lock(imePtr->mtx);
+        if(imePtr->input.empty()){
+            return;
+        }
+
+        if(imePtr->stk.empty()){
+            imePtr->input.pop_back();
+        }
+        else{
+            imePtr->stk.pop_back();
+        }
+    }
+    imePtr->cond.notify_one();
+}
+
+void IME::assign(std::string argPrefix, std::string argInput)
+{
+    const auto imePtr = asIMEPtr(m_instance);
+    {
+        const std::lock_guard<std::mutex> lock(imePtr->mtx);
+        imePtr->stk.clear();
+        imePtr->selection.reset();
+
+        imePtr->prefix = std::move(argPrefix);
+        imePtr->input  = std::move(argInput);
+    }
+    imePtr->cond.notify_one();
+}
+
+void IME::select(size_t index)
+{
+    const auto imePtr = asIMEPtr(m_instance);
+    {
+        const std::lock_guard<std::mutex> lock(imePtr->mtx);
+        imePtr->selection = index;
+    }
+    imePtr->cond.notify_one();
+}
+
+std::string IME::input() const
+{
+    const auto imePtr = asIMEPtr(m_instance);
+    const std::lock_guard<std::mutex> lock(imePtr->mtx);
+    return imePtr->input;
+}
+
+std::string IME::result() const
+{
+    const auto imePtr = asIMEPtr(m_instance);
+    const std::lock_guard<std::mutex> lock(imePtr->mtx);
+    if(imePtr->stk.empty()){
+        return imePtr->input;
+    }
+    return imePtr->stk.back().first + imePtr->input.substr(imePtr->stk.back().second);
+}
+
+std::string IME::sentence() const
+{
+    const auto imePtr = asIMEPtr(m_instance);
+    const std::lock_guard<std::mutex> lock(imePtr->mtx);
+    if(imePtr->stk.empty()){
+        return {};
+    }
+    return imePtr->stk.back().first;
+}
+
+std::vector<std::string> IME::candidateList() const
+{
+    const auto imePtr = asIMEPtr(m_instance);
+    const std::lock_guard<std::mutex> lock(imePtr->mtx);
+    return imePtr->candidateList;
 }
--- client/src/ime.hpp
+++ client/src/ime.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include <vector>
 #include <string>
+#include <cstdint>
+#include <utility>
 
 class IME
 {
@@ -14,5 +16,19 @@ class IME
         virtual ~IME();
 
     public:
-        std::vector<std::string> getCandidateStringList() const;
+        void clear();
+
+    public:
+        void feed(char);
+        void backspace();
+        void assign(std::string, std::string);
+
+    public:
+        void select(size_t);
+
+    public:
+        std::string input() const;
+        std::string result() const;
+        std::string sentence() const;
+        std::vector<std::string> candidateList() const;
 };
