41aefd97e09ded4ecf96cc6cfcc6b3dcfc49b7d5
etorth <anhonghe@gmail.com>
2023-03-02 16:37:30 -0800

    f

--- cmake/download/debugger.cmake
+++ cmake/download/debugger.cmake
@@ -3,7 +3,7 @@ IF(NOT EXISTS "${MIR2X_3RD_PARTY_DIR}/download/debugger/debugger.lua")
     FILE(DOWNLOAD "https://raw.githubusercontent.com/slembcke/debugger.lua/master/debugger.lua" "${MIR2X_3RD_PARTY_DIR}/download/debugger/debugger.lua")
 ENDIF()
 
-MESSAGE(STATUS "download debugger.lua, in ${MIR2X_3RD_PARTY_DIR}/download/debugger")
+MESSAGE(STATUS "downloaded debugger.lua, in ${MIR2X_3RD_PARTY_DIR}/download/debugger")
 
 INSTALL(FILES ${MIR2X_3RD_PARTY_DIR}/download/debugger/debugger.lua DESTINATION server/script/3rdparty)
 INSTALL(FILES ${MIR2X_3RD_PARTY_DIR}/download/debugger/debugger.lua DESTINATION client/script/3rdparty)
