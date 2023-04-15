5a2fef3d34e81e59c39d660d69ca6c0aead109b8
etorth <anhonghe@gmail.com>
2023-04-14 20:00:58 -0700

    an example of sendNotify

--- server/script/quest/万事通的烦恼.lua
+++ server/script/quest/万事通的烦恼.lua
@@ -1,22 +1,58 @@
 function main()
     uidExecute(getNPCharUID('道馆_1', '万事通_1'),
     [[
-        return setQuestHandler('%s', {
-            ['%s'] = function(uid)
+        local questName = '%s'
+        local questPath = {SYS_EPQST, questName}
+
+        return setQuestHandler(questName, {
+            [SYS_CHECKACTIVE] = function(uid)
                 return uidExecute(uid, [=[ return getLevel() ]=]) >= 7
             end,
 
-            ['%s'] = function(uid, value)
-                uidPostXML(uid, {'%s', '%s'}, [=[
+            [SYS_ENTER] = function(uid, value)
+                uidPostXML(uid, questPath, [=[
                     <layout>
                         <par>我万拍子近日愁眉苦脸，都是被那城门外的钉耙猫害的啊！少侠你已经%%d级了，愿意帮助老夫吗？</par>
                         <par><event id="npc_goto_1">同意</event></par>
-                        <par><event id="npc_goto_2">拒绝</event></par>
-                        <par></par>
-                        <par><event id="%s">退出</event></par>
+                        <par><event id="%%s"       >退出</event></par>
                     </layout>
-                ]=], uidExecute(uid, [=[ return getLevel() ]=]))
+                ]=], uidExecute(uid, [=[ return getLevel() ]=]), SYS_EXIT)
+            end,
+
+            ['npc_goto_1'] = function(uid, value)
+                uidExecute(uid, [=[
+                    addTrigger(SYS_ON_KILL, function(monsterID)
+                        if getMonsterName(monsterID) == '钉耙猫' then
+                            postString('已经消灭钉耙猫，任务【%%s】已经更新！')
+                            sendNotify(%d, %d, %d, %%d)
+                            return true
+                        end
+                    end)
+                ]=], questName, uid)
+
+                uidPostXML(uid, questPath, [=[
+                    <layout>
+                        <par>这是一个测试。</par>
+                        <par>杀死一只钉耙猫，任务<t color="red">【%%s】</t>将进入下一步。</par>
+                        <par></par>
+                        <par><event id="%%s">退出</event></par>
+                    </layout>
+                ]=], questName, SYS_EXIT)
             end
         })
-    ]], getQuestName(), SYS_CHECKACTIVE, SYS_ENTER, SYS_EPQST, getQuestName(), SYS_EXIT)
+    ]],
+
+    getQuestName(),
+    getUID(), getTLSTable().threadKey, getTLSTable().threadSeqID)
+
+    local notifies = waitNotify(1)
+    local playerUID = notifies[1][1]
+
+    uidExecute(playerUID,
+    [[
+        postString('任务协程确认：任务【%s】被%s恢复')
+    ]],
+
+    getQuestName(),
+    uidExecute(playerUID, [[ return getName() ]]));
 end
