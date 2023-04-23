749057b3f42badbafa97662c2dd6c21f9eccac31
etorth <anhonghe@gmail.com>
2023-04-23 14:20:57 -0700

    f

--- client/src/teamstateboard.cpp
+++ client/src/teamstateboard.cpp
@@ -331,7 +331,8 @@ bool TeamStateBoard::processEvent(const SDL_Event &event, bool valid)
     switch(event.type){
         case SDL_MOUSEBUTTONDOWN:
             {
-                for(size_t i = 0; i < lineCount(); ++i){
+                m_selectedIndex = -1; // reset if click on invalid line
+                for(size_t i = 0; (i < lineCount()) && (i + m_startIndex < m_uidList.size()); ++i){
                     const auto lineX = x() + m_uidRegionX;
                     const auto lineY = y() + m_uidRegionY + i * lineHeight();
 
@@ -340,6 +341,7 @@ bool TeamStateBoard::processEvent(const SDL_Event &event, bool valid)
                         if(event.button.clicks >= 2){
                             // can show hero state board here
                         }
+                        break;
                     }
                 }
                 return consumeFocus(true);
