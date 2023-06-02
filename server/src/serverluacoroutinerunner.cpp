b7c8b9225c0b3f498f561005a6bf11887160a181
etorth <anhonghe@gmail.com>
2023-06-02 10:27:37 -0700

    fix close var

--- server/src/serverluacoroutinerunner.cpp
+++ server/src/serverluacoroutinerunner.cpp
@@ -208,13 +208,13 @@ uint64_t ServerLuaCoroutineRunner::spawn(uint64_t key, const std::string &code, 
     const auto currSeqID = p->second->seqID;
 
     resumeRunner(p->second.get(), str_printf(
-        R"###( getTLSTable().threadKey = %llu                           )###""\n"
-        R"###( getTLSTable().threadSeqID = %llu                         )###""\n"
-        R"###( getTLSTable().startTime = getNanoTstamp()                )###""\n"
-        R"###( local _RSVD_NAME_autoTLSTableClear = autoClearTLSTable() )###""\n"
-        R"###( do                                                       )###""\n"
-        R"###(    %s                                                    )###""\n"
-        R"###( end                                                      )###""\n", to_llu(key), to_llu(currSeqID), code.c_str()));
+        R"###( getTLSTable().threadKey = %llu                                  )###""\n"
+        R"###( getTLSTable().threadSeqID = %llu                                )###""\n"
+        R"###( getTLSTable().startTime = getNanoTstamp()                       )###""\n"
+        R"###( local _RSVD_NAME_autoTLSTableClear<close> = autoClearTLSTable() )###""\n"
+        R"###( do                                                              )###""\n"
+        R"###(    %s                                                           )###""\n"
+        R"###( end                                                             )###""\n", to_llu(key), to_llu(currSeqID), code.c_str()));
 
     return currSeqID; // don't use p resumeRunner() can invalidate p
 }
