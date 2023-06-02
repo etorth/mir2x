32da54f4e241e658f54c657cbac48772d54d7b73
etorth <anhonghe@gmail.com>
2023-06-02 14:15:56 -0700

    f

--- server/script/quest/队友合作.lua
+++ server/script/quest/队友合作.lua
@@ -1,6 +1,7 @@
 function main()
     uidExecute(getNPCharUID('道馆_1', '士官_1'),
     [[
+        local questUID  = %d
         local questName = '%s'
         local questPath = {SYS_EPQST, questName}
 
@@ -27,15 +28,52 @@ function main()
                     ]=], SYS_EXIT)
 
                 else
-                    uidPostXML(uid, questPath,
-                    [=[
-                        <layout>
-                            <par>你拥有一个队伍</par>
-                            <par><event id="%%s">退出</event></par>
-                        </layout>
-                    ]=], SYS_EXIT)
+                    local teamMemberList = uidExecute(uid, [=[ return getTeamMemberList() ]=])
+                    if #teamMemberList >= 2 then
+                        uidPostXML(uid, questPath,
+                        [=[
+                            <layout>
+                                <par>你拥有一个队伍，愿意接受任务吗？</par>
+                                <par><event id="npc_accept_quest">同意</event></par>
+                                <par><event id="%%s"             >退出</event></par>
+                            </layout>
+                        ]=], SYS_EXIT)
+                    else
+                        uidPostXML(uid, questPath,
+                        [=[
+                            <layout>
+                                <par>你的队伍中只有你自己，请至少添加一名队友。</par>
+                                <par><event id="%%s">退出</event></par>
+                            </layout>
+                        ]=], SYS_EXIT)
+                    end
                 end
             end,
+
+            npc_accept_quest = function(uid, value)
+                uidExecute(questUID,
+                [=[
+                    setQuestState(%%d, SYS_ENTER)
+                ]=], uid)
+            end,
         })
-    ]], getQuestName())
+    ]], getUID(), getQuestName())
+
+    setQuestFSMTable(
+    {
+        [SYS_ENTER] = function(uid)
+            uidExecute(getNPCharUID('道馆_1', '士官_1'),
+            [[
+                uidPostXML(%d,
+                [=[
+                    <layout>
+                        <par>和队友开始挑战珐玛大陆的怪物吧！</par>
+                        <par></par>
+                        <par><event id="%%s">好的</event></par>
+                    </layout>
+                ]=], SYS_EXIT)
+
+            ]], uid)
+        end,
+    })
 end
