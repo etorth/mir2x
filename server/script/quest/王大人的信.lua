6cab5cd91a7489180870a752f26ddcf49b30eca7
etorth <anhonghe@gmail.com>
2023-06-11 21:49:26 -0700

    f

--- server/script/quest/王大人的信.lua
+++ server/script/quest/王大人的信.lua
@@ -1,15 +1,20 @@
 function main()
     uidExecute(getNPCharUID('比奇县_0', '王大人_1'),
     [[
-        return setQuestHandler('%s', {
+        local questName = '%s'
+        local questPath = {SYS_EPQST, questName}
+
+        return setQuestHandler(questName,
+        {
             [SYS_ENTER] = function(uid, value)
-                uidPostXML(uid, {SYS_EPQST, '%s'}, [=[
+                uidPostXML(uid, questPath,
+                [=[
                     <layout>
                         <par>这是一个测试</par>
-                        <par><event id="%s">退出</event></par>
+                        <par><event id="%%s">退出</event></par>
                     </layout>
-                ]=])
+                ]=], SYS_EXIT)
             end
         })
-    ]], getQuestName(), getQuestName(), SYS_EXIT)
+    ]], getQuestName())
 end
