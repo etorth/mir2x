2cd477f9b402af7cff5f5f1373d0891928b24d54
etorth <anhonghe@gmail.com>
2023-06-26 15:10:06 -0700

    f

--- server/script/quest/初出江湖.lua
+++ server/script/quest/初出江湖.lua
@@ -91,7 +91,7 @@ function main()
                     uidPostXML(uid, questPath,
                     [=[
                         <layout>
-                            <par>这个，嗯，详细的情况请到收罗杂货的<t color="red">大老板道友</t>那儿打听吧。</par>
+                            <par>这个，嗯，详细的情况请到收罗杂货的<t color="red">大老板</t>道友那儿打听吧。</par>
                             <par>大老板道友就在道馆内。从这往下走，在右侧可以看到杂货店，进去就可以见到他了。杂货店入口的大概位置在<event id="quest_fly_to_loc" arg="{394,169}">(394,169)</event>，请参考一下吧！</par>
                             <par></par>
                             <par><event id="%%s">好的</event></par>
