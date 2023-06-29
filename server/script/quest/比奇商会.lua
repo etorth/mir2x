a4b905634e31ee1c5a3532385edf274213952ab3
etorth <anhonghe@gmail.com>
2023-06-29 15:23:11 -0700

    f

--- server/script/quest/比奇商会.lua
+++ server/script/quest/比奇商会.lua
@@ -1,6 +1,7 @@
 function main()
     uidExecute(getNPCharUID('比奇县_0', '王大人_1'),
     [[
+        local questUID  = %d
         local questName = '%s'
         local questPath = {SYS_EPQST, questName}
 
@@ -28,35 +29,49 @@ function main()
                             <par>噢！你来啦！你要是不来我正好要派人去叫你呢？其实我是有事情要拜托你做！是这样的！最近以我为会长的比奇商会和以那个姓崔的贪得无厌的家伙为首的传奇商会正在互相争夺势力。</par>
                             <par>啊！当然不是真刀真枪的动武啦！是为了争夺商权而展开的势力之争。不管怎么样，胜者为王败者寇，在这场斗争中失败者将被排挤出比奇省。因此想要求你帮我办点事情，为了增强我们比奇商会的势力，请你去说服图书管理人和施药商药剂师从传奇商会中退出，来加入我们比奇商会。</par>
                             <par></par>
-                            <par><event id="npc_accept_quest">好的！</event></par>
-                            <par><event id="npc_refuse_quest">我还有别的事情要做。</event></par>
+                            <par><event id="npc_decide" args="true" >好的！</event></par>
+                            <par><event id="npc_decide" args="false">我还有别的事情要做。</event></par>
                         </layout>
                     ]=])
                 end
             end,
 
-            npc_accept_quest = function(uid, value)
-                uidPostXML(uid, questPath,
-                [=[
-                    <layout>
-                        <par>妈耶！你来辣！</par>
-                        <par></par>
-                        <par><event id="%%s">结束</event></par>
-                    </layout>
-                ]=], SYS_EXIT)
-            end,
-
-            npc_refuse_quest = function(uid, value)
-                uidPostXML(uid, questPath,
-                [=[
-                    <layout>
-                        <par>不行？</par>
-                        <par>那么我只好再去找其他人了。</par>
-                        <par></par>
-                        <par><event id="%%s">结束</event></par>
-                    </layout>
-                ]=], SYS_EXIT)
+            npc_decide = function(uid, value)
+                uidExecute(questUID, [=[ setUIDQuestState(%%d, SYS_ENTER, %%s) ]=], uid, value)
             end,
         })
-    ]], getQuestName())
+    ]], getUID(), getQuestName())
+
+    setQuestFSMTable(
+    {
+        [SYS_ENTER] = function(uid, value)
+            uidExecute(getNPCharUID('比奇县_0', '王大人_1'),
+            [[
+                local accepted  = %s
+                local questName = '%s'
+                local questPath = {SYS_EPQST, questName}
+
+                if acceptedQuest then
+                    uidPostXML(uid, questPath,
+                    [=[
+                        <layout>
+                            <par>妈耶！你来辣！</par>
+                            <par></par>
+                            <par><event id="%%s">结束</event></par>
+                        </layout>
+                    ]=], SYS_EXIT)
+                else
+                    uidPostXML(uid, questPath,
+                    [=[
+                        <layout>
+                            <par>不行？</par>
+                            <par>那么我只好再去找其他人了。</par>
+                            <par></par>
+                            <par><event id="%%s">结束</event></par>
+                        </layout>
+                    ]=], SYS_EXIT)
+                end
+            ]], value, getQuestName())
+        end,
+    })
 end
