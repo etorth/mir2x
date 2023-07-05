55297032d229bea63496c597e3195fe48c23076d
etorth <anhonghe@gmail.com>
2023-07-04 18:58:38 -0700

    f

--- server/script/quest/比奇商会.lua
+++ server/script/quest/比奇商会.lua
@@ -140,6 +140,34 @@ function main()
                                 <par>最近比奇省里流行传染病，配制治疗这种病的药所需的原料毒蛇牙齿非常的紧缺。这种毒蛇牙齿在毒蛇山谷村就有卖的，但是我现在马上要给源源不断而来的病人治病，没有去买药材的时间。</par>
                                 <par>传奇商会那帮人唯利是图，人命关天的事却无人愿意搭把手帮助我。如果你能够买来足够我们所需的毒蛇牙齿，我就会抛开商人的身份来以医生的角度听从您的劝说。</par>
                                 <par></par>
+                                <par><event id="npc_discuss_accepted">没问题！</event></par>
+                                <par><event id="npc_discuss_refused">请给我点儿考虑的时间。</event></par>
+                            </layout>
+                        ]=], SYS_EXIT)
+                    end,
+
+                    npc_discuss_accepted = function(uid, value)
+                        uidPostXML(uid, questPath,
+                        [=[
+                            <layout>
+                                <par>从这儿向东北部去就能到达毒蛇山谷，可能去(643,15)附近就能够找得到。</par>
+                                <par>穿过毒蛇山谷一直向东走就会达到那个村庄。在那儿找药商<t color="red">金中医</t>(334,224)向他购买<t color="red">毒蛇牙齿</t>。</par>
+                                <par>现在患者数量仍然呈增加的趋势，所以还不能推测出以后具体需要多少药材。不管怎么样你都要快去快回。</par>
+                                <par></par>
+                                <par><event id="%%s">没问题！</event></par>
+                                <par><event id="%%s">给我点儿考虑的时间。</event></par>
+                            </layout>
+                        ]=], SYS_EXIT, SYS_EXIT)
+                    end,
+
+                    npc_discuss_refused = function(uid, value)
+                        uidPostXML(uid, questPath,
+                        [=[
+                            <layout>
+                                <par>明白吗，年轻人？！</par>
+                                <par>千万不要太拖延而忘了一切啊！人命关天啊！</par>
+                                <par>我们所有人啊！</par>
+                                <par></par>
                                 <par><event id="%%s">没问题！</event></par>
                                 <par><event id="%%s">给我点儿考虑的时间。</event></par>
                             </layout>
