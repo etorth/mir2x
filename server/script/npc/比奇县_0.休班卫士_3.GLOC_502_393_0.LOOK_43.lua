b991087be047296adcd0c2619389d644d09404f5
etorth <anhonghe@gmail.com>
2023-07-09 01:51:42 -0700

    f

---
+++ server/script/npc/比奇县_0.休班卫士_3.GLOC_502_393_0.LOOK_43.lua
@@ -0,0 +1,14 @@
+setEventHandler(
+{
+    [SYS_ENTER] = function(uid, value)
+        uidPostXML(uid,
+        [[
+            <layout>
+                <par>我是这比奇省里资历最深的卫士。</par>
+                <par>嗯...虽说什么，那也不是特别的...</par>
+                <par></par>
+                <par><event id="%s">关闭</event></par>
+            </layout>
+        ]], SYS_EXIT)
+    end,
+})
