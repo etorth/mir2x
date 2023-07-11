b22dbcc049b6451c5120e7e171e479cf5890a8b9
etorth <anhonghe@gmail.com>
2023-07-10 19:55:30 -0700

    f

---
+++ server/script/quest/脚本调试工具人.lua
@@ -0,0 +1,54 @@
+function main()
+    uidRemoteCall(getNPCharUID('道馆_1', '物品展示商人'), getUID(), getQuestName(),
+    [[
+        local questUID, questName = ...
+        local questPath = {SYS_EPQST, questName}
+
+        setQuestHandler(questName,
+        {
+            [SYS_ENTER] = function(uid, value)
+                uidPostXML(uid, questPath,
+                [=[
+                    <layout>
+                        <par>你好？</par>
+                        <par>我可以帮你测试脚本功能。</par>
+                        <par></par>
+                        <par><event id="npc_test_script">测试脚本</event></par>
+                        <par><event id="%s">退出</event></par>
+                    </layout>
+                ]=], SYS_EXIT)
+            end,
+
+            npc_test_script = function(uid, value)
+                uidRemoteCall(questUID, uid, [=[ setUIDQuestState(..., SYS_ENTER)]=])
+            end
+        })
+    ]])
+
+    setQuestFSMTable(
+    {
+        [SYS_ENTER] = function(uid, value)
+            setupNPCQuestBehavior('道馆_1', '物品展示商人', uid,
+            [[
+                return getUID(), getQuestName()
+            ]],
+            [[
+                local questUID, questName = ...
+                local questPath = {SYS_EPUID, questName}
+                return
+                {
+                    [SYS_ENTER] = function(uid, value)
+                        uidPostXML(uid, questPath,
+                        [=[
+                            <layout>
+                                <par>你是来测试脚本的吗？</par>
+                                <par></par>
+                                <par><event id="%s">退出</event></par>
+                            </layout>
+                        ]=], SYS_EXIT)
+                    end,
+                }
+            ]])
+        end,
+    })
+end
