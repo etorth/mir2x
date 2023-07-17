c41ca78514d9ee6eaaaba442777af359cd5cdd6e
etorth <anhonghe@gmail.com>
2023-07-17 00:19:08 -0700

    f

--- server/src/serverluacoroutinerunner.hpp
+++ server/src/serverluacoroutinerunner.hpp
@@ -233,7 +233,7 @@ class ServerLuaCoroutineRunner: public ServerLuaModule
                 , seqID(argSeqID)
                 , onDone(std::move(argOnDone))
                 , runner(sol::thread::create(argLuaModule.getLuaState().lua_state()))
-                , callback(runner.state(), sol::ref_index(func.registry_index()))
+                , callback(runner.state(), sol::ref_index(func.registry_index())) // callback initialized as: https://github.com/ThePhD/sol2/issues/836
             {
                 fflassert(key);
                 fflassert(seqID);
