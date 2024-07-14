30d3fc4867f373ff2234239ca6dfed2eae5d7fab
etorth <anhonghe@gmail.com>
2024-07-15 00:17:21 +0800

    fix ChatItemContainer::append

--- client/src/friendchatboard_chatitemcontainer.cpp
+++ client/src/friendchatboard_chatitemcontainer.cpp
@@ -55,58 +55,64 @@ FriendChatBoard::ChatItemContainer::ChatItemContainer(dir8_t argDir,
 
 void FriendChatBoard::ChatItemContainer::append(const SDChatMessage &sdCM, std::function<void(const FriendChatBoard::ChatItem *)> fnOp)
 {
-    FriendChatBoard::getParentBoard(this)->queryChatPeer(sdCM.from, [sdCM, fnOp = std::move(fnOp), this](const SDChatPeer *peer, bool)
+    auto chatItem = new ChatItem
     {
-        if(!peer){
-            return;
-        }
+        DIR_UPLEFT,
+        0,
+        0,
 
-        const auto chatPage = dynamic_cast<ChatPage *>(parent());
-        const auto self = FriendChatBoard::getParentBoard(this)->m_processRun->getMyHero();
+        !sdCM.seq.has_value(),
 
-        if(sdCM.from != chatPage->peer.cpid() && sdCM.to != chatPage->peer.cpid()){
-            fnOp(nullptr);
-            return;
-        }
+        to_u8cstr("NA"),
+        to_u8cstr(cerealf::deserialize<std::string>(sdCM.message)),
 
-        auto chatItem = new ChatItem
+        [](const ImageBoard *)
         {
-            DIR_UPLEFT,
-            0,
-            0,
+            return g_progUseDB->retrieve(0X010007CF);
+        },
 
-            !sdCM.seq.has_value(),
+        sdCM.from != FriendChatBoard::getParentBoard(this)->m_processRun->getMyHeroChatPeer().cpid(),
+        sdCM.from != FriendChatBoard::getParentBoard(this)->m_processRun->getMyHeroChatPeer().cpid(),
 
-            to_u8cstr(peer->name),
-            to_u8cstr(cerealf::deserialize<std::string>(sdCM.message)),
+        {},
+    };
 
-            [from = sdCM.from, gender = peer->player() ? peer->player()->gender : false, job = peer->player() ? peer->player()->job : 0](const ImageBoard *)
+    const auto startY = canvas.hasChild() ? (canvas.h() + ChatItem::ITEM_SPACE) : 0;
+
+    if(chatItem->avatarLeft){
+        chatItem->moveAt(DIR_UPLEFT, 0, startY);
+    }
+    else{
+        chatItem->moveAt(DIR_UPRIGHT, canvas.w() - 1, startY);
+    }
+
+    canvas.addChild(chatItem, true);
+    dynamic_cast<ChatPage *>(parent())->placeholder.setShow(false);
+
+    FriendChatBoard::getParentBoard(this)->queryChatPeer(sdCM.from, [widgetID = chatItem->id(), sdCM, fnOp = std::move(fnOp), this](const SDChatPeer *peer, bool)
+    {
+        fflassert(peer, sdCM.from.asU64());
+        if(auto chatItem = dynamic_cast<FriendChatBoard::ChatItem *>(this->canvas.hasChild(widgetID))){
+            const auto from = sdCM.from;
+            const auto job    = peer->player() ? peer->player()->job    : 0;
+            const auto gender = peer->player() ? peer->player()->gender : false;
+
+            chatItem->name.setText(to_u8cstr(peer->name));
+            chatItem->avatar.setLoadFunc([from, job, gender](const ImageBoard *)
             {
                 if     (from == SDChatPeerID(CP_SPECIAL, SYS_CHATDBID_SYSTEM)) return g_progUseDB->retrieve(0X00001100);
                 else if(from == SDChatPeerID(CP_SPECIAL, SYS_CHATDBID_GROUP )) return g_progUseDB->retrieve(0X00001300);
                 else                                                           return g_progUseDB->retrieve(Hero::faceGfxID(gender, job));
-            },
+            });
 
-            peer->id != self->dbid(),
-            peer->id != self->dbid(),
-
-            {},
-        };
-
-        const auto startY = canvas.hasChild() ? (canvas.h() + ChatItem::ITEM_SPACE) : 0;
-
-        if(chatItem->avatarLeft){
-            chatItem->moveAt(DIR_UPLEFT, 0, startY);
+            if(fnOp){
+                fnOp(chatItem);
+            }
         }
         else{
-            chatItem->moveAt(DIR_UPRIGHT, canvas.w() - 1, startY);
+            if(fnOp){
+                fnOp(nullptr);
+            }
         }
-
-        canvas.addChild(chatItem, true);
-        if(fnOp){
-            fnOp(chatItem);
-        }
-
-        dynamic_cast<ChatPage *>(parent())->placeholder.setShow(false);
     });
 }
