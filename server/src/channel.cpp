c5ca8ab010e9e8267c1f3fc227c9c20a70f921f8
etorth <anhonghe@gmail.com>
2025-03-11 15:49:14 -0700

    f

--- server/src/channel.cpp
+++ server/src/channel.cpp
@@ -190,8 +190,15 @@ void Channel::close()
 void Channel::launch()
 {
     fflassert(g_netDriver->isNetThread());
-    asio::co_spawn(m_socket.get_executor(), [self = shared_from_this()]{ return self->reader(); }, +[](std::exception_ptr e){ std::rethrow_exception(e); });
-    asio::co_spawn(m_socket.get_executor(), [self = shared_from_this()]{ return self->writer(); }, +[](std::exception_ptr e){ std::rethrow_exception(e); });
+    const auto handler = +[](std::exception_ptr e)
+    {
+        if(e){
+            std::rethrow_exception(e);
+        }
+    };
+
+    asio::co_spawn(m_socket.get_executor(), [self = shared_from_this()]{ return self->reader(); }, handler);
+    asio::co_spawn(m_socket.get_executor(), [self = shared_from_this()]{ return self->writer(); }, handler);
 }
 
 void Channel::bindPlayer(uint64_t uid)
