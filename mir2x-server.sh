05c0693f8a1188281d98d71180edd1460700e9e1
etorth <anhonghe@gmail.com>
2025-05-28 15:46:38 +0800

    f

--- mir2x-server.sh
+++ mir2x-server.sh
@@ -16,12 +16,8 @@ echo "Profiling result generated at: ${LOG_DIR}"
 # menas we need to rebuild the whole project
 # otherwise the profiler also samples libasan for result
 
-(\
-    cd ${BIN_DIR};  \
-    env             \
-    CPUPROFILE=${LOG_DIR}/mir2x-server.log  \
-    LD_PRELOAD="/usr/lib/libprofiler.so"    \
-    ./server                                \
-)
+cd ${BIN_DIR};
+env CPUPROFILE=${LOG_DIR}/mir2x-server.log LD_PRELOAD="/usr/lib/x86_64-linux-gnu/libtcmalloc_and_profiler.so.4.6.11" ./server --auto-launch --disable-monster-idle-wait
 
+cd ${LOG_DIR};
 google-pprof --svg ${BIN_DIR}/server ${LOG_DIR}/mir2x-server.log > ${LOG_DIR}/mir2x-server.svg
