5c140e7919abbb63ade054017cbc562bd506109f
etorth <anhonghe@gmail.com>
2025-06-05 17:16:15 +0800

    f

--- mir2x-server-cpu-profiler.sh
+++ mir2x-server-cpu-profiler.sh
@@ -1,19 +1,41 @@
 #!/bin/bash
 
-if [[ $# != 1 ]]
+if [[ $# < 1 ]]
 then
-    echo "Usage: $0 install-path"
+    echo "Usage: $0 server-bin-path [options]"
     exit 1
 fi
 
-BIN_DIR=$1
-LOG_DIR=${PWD}
+if [[ ! -f "$1" ]]
+then
+    echo "Error: invalid server binary: $1"
+    exit 1
+fi
 
-echo "Profiling server installed at: ${BIN_DIR}"
+LOG_DIR=${PWD}
+BIN_DIR=$(dirname "$1")
+BIN_NAME=$(basename "$1")
+
+shift
+
+if [[ ! -d "${BIN_DIR}" ]]
+then
+    echo "Error: invalid server-install-path: ${BIN_DIR}"
+    exit 1
+fi
+
+
+if [[ -z "${PROF_LIB_PATH}" || ! -f "${PROF_LIB_PATH}" ]]
+then
+    echo "Error: PROF_LIB_PATH is not set or does not point to a valid directory"
+    exit 1
+fi
+
 echo "Profiling result generated at: ${LOG_DIR}"
+echo "Profiling server installed at: ${BIN_DIR}"
 
 cd ${BIN_DIR};
-env CPUPROFILE=${LOG_DIR}/server.cpu.log LD_PRELOAD="/usr/lib/x86_64-linux-gnu/libtcmalloc_and_profiler.so.4.6.11" ./server --auto-launch --disable-monster-idle-wait
+env CPUPROFILE=${LOG_DIR}/server.cpu.log LD_PRELOAD="${PROF_LIB_PATH}" ./${BIN_NAME} "$@"
 
 cd ${LOG_DIR};
-google-pprof --svg ${BIN_DIR}/server ${LOG_DIR}/mir2x-server.log > ${LOG_DIR}/mir2x-server.svg
+google-pprof --svg ${BIN_DIR}/server ${LOG_DIR}/server.cpu.log > ${LOG_DIR}/server.cpu.svg
